3bda9377c115ba0bd834f131f31d85bac0970246
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2024-02-16 01:43:42 +0530

    Time: 81 ms (46.87%) | Memory: 50.1 MB (13.06%) - LeetSync

---
+++ 2413-smallest-number-in-infinite-set/smallest-number-in-infinite-set.cpp
@@ -0,0 +1,18 @@
+class SmallestInfiniteSet {
+public:
+    set<int> st;
+    SmallestInfiniteSet() {
+        for(int i =1;i<=1000;i++) st.insert(i);
+    }
+    
+    int popSmallest() {
+        int a = *st.begin();
+        st.erase(a);
+        return a;
+    }
+    
+    void addBack(int num) {
+        if(st.find(num)==st.end())
+        st.insert(num);
+    }
+};
