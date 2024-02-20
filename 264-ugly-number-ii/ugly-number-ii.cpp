4b5e8c207ad218f630483e0592d633edae839002
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2024-02-20 12:13:34 +0530

    Time: 7 ms (58.14%) | Memory: 9.2 MB (42.12%) - LeetSync

--- 264-ugly-number-ii/ugly-number-ii.cpp
+++ 264-ugly-number-ii/ugly-number-ii.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        if (n <= 0) return 0;
-        vector<int> ugly(n);
-        ugly[0] = 1;
-        int p2 = 0, p3 = 0, p5 = 0;
-        int next_multiple_of_2 = 2;
-        int next_multiple_of_3 = 3;
-        int next_multiple_of_5 = 5;
-        
-        for (int i = 1; i < n; ++i) {
-            int next_ugly = min(next_multiple_of_2, min(next_multiple_of_3, next_multiple_of_5));
-            ugly[i] = next_ugly;
-            if (next_ugly == next_multiple_of_2) {
-                p2++;
-                next_multiple_of_2 = ugly[p2] * 2;
-            }
-            if (next_ugly == next_multiple_of_3) {
-                p3++;
-                next_multiple_of_3 = ugly[p3] * 3;
-            }
-            if (next_ugly == next_multiple_of_5) {
-                p5++;
-                next_multiple_of_5 = ugly[p5] * 5;
-            }
+        vector<int>dp(n);
+        int p2=0,p3=0,p5=0;
+        dp[0]=1;
+        for(int i=1;i<n;i++)
+        { 
+            int x= 2*dp[p2];
+            int y= 3*dp[p3];
+            int z= 5*dp[p5];
+           dp[i]=min(x,min(y,z));
+
+           if(dp[i]==x) p2++;
+           if(dp[i]==y) p3++;
+           if(dp[i]==z) p5++;
+
         }
-        return ugly[n - 1];
+        return dp[n-1];
     }
 };
