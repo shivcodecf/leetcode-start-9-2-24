49b042338765daa6ca079851b2a5b47633acf45a
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2024-02-21 17:20:04 +0530

    Time: 2 ms (38.45%) | Memory: 7.4 MB (5.58%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -1,27 +1,8 @@
 class Solution {
-public: 
-
-    bool solve(int n) {   
-        if (n == 1) {
-            return true;
-        }
-
-        if (n % 2 != 0) {
-            return false;
-        }
-
-        // Return the result of the recursive call
-        return solve(n / 2);
-    }
-
+public:
     bool isPowerOfTwo(int n) {
-        if (n == 1) {
-            return true;
-        }
-        if(n==0)
-        {
-            return false;
-        }
-        return solve(n);
+         
+         if(n<=0) return false;
+        return ((n&(n-1))==0);
     }
 };
