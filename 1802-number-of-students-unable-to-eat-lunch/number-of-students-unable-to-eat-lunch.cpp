01eb2234b1fbfb37c1beb410db8b908045834713
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2024-04-09 00:34:48 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (95.64%) - LeetSync

---
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.cpp
@@ -0,0 +1,39 @@
+#include <vector>
+#include <queue>
+using namespace std;
+
+class Solution {
+public:
+    int countStudents(vector<int>& students, vector<int>& sandwiches) {
+        queue<int> q;
+        int n = students.size();
+        for (int i = 0; i < n; ++i)
+            q.push(students[i]);
+        
+        
+        for (int i = 0; i < sandwiches.size(); ++i) {
+            int currentSandwich = sandwiches[i];
+            int originalSize = q.size(); 
+            for(int j=0;j<originalSize;j++)
+            {      
+                int front=q.front();
+                q.pop();
+              if( front != currentSandwich ) {
+               q.push(front);
+              
+               }
+               else {
+                
+                break;
+               }
+
+            }
+           if(originalSize == q.size())
+           {
+            return originalSize;
+           }
+        }
+        
+        return 0; // All sandwiches served
+    }
+};
