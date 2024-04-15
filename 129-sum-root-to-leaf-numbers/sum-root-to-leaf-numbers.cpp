e3702db66c29100dcd4ad2cf31b25b09a186d9b4
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2024-04-15 23:59:23 +0530

    Time: 2 ms (44.34%) | Memory: 11 MB (52.64%) - LeetSync

---
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
@@ -0,0 +1,39 @@
+class Solution {
+
+    void solve(TreeNode* curr,int sum,int& totalsum){
+
+        int temp = 0;
+
+        sum = sum*10+curr->val;
+
+        if(curr->left == NULL && curr->right==NULL){
+
+            totalsum += sum;   
+            return;
+        }
+        if(curr->left){
+
+            solve(curr->left,sum,totalsum);
+
+        }
+        if(curr->right){
+
+            solve(curr->right,sum,totalsum);
+
+        }
+
+        sum = temp;
+
+
+    }
+
+
+ public:
+
+    int sumNumbers(TreeNode* root) {
+        int totalsum=0;
+        solve(root,0,totalsum);
+        return totalsum;
+    }
+
+};
