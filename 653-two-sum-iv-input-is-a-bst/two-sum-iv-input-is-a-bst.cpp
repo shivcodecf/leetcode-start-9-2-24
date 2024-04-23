78c79fd9f5231e07bbf1ed6fb058336b9ea1b0dc
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2024-04-23 17:22:08 +0530

    Time: 45 ms (6.82%) | Memory: 39.6 MB (11.78%) - LeetSync

---
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.cpp
@@ -0,0 +1,61 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> mp;
+    void traveral(TreeNode* root)
+    {
+        if(root==NULL)
+        {
+            return;
+        }
+        traveral(root->left);
+
+        mp.push_back(root->val);
+
+        traveral(root->right);
+    }
+
+    bool findTarget(TreeNode* root, int k) {
+
+          traveral(root);
+
+          map<int,int> fin;
+
+        for(int i=0;i<mp.size();i++)
+        { 
+
+            fin.insert({i,mp[i]});
+
+        }
+
+
+        for(int i=0;i<mp.size()-1;i++)
+        {
+            int x= mp[i];
+
+            int target =k-x;
+
+            for(int j=i+1;j<mp.size();j++)
+            {
+                if(mp[j]==target )
+                {
+                    return true;
+                }
+            }
+        }
+
+
+
+        return false;
+    }
+};
