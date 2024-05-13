6a588922c27bdc3e3a74e25028b0c874aa857f7d
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2024-05-13 20:01:51 +0530

    Time: 637 ms (19.58%) | Memory: 228.4 MB (13.76%) - LeetSync

---
+++ 802-k-th-smallest-prime-fraction/k-th-smallest-prime-fraction.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
+
+       
+        vector<pair<double,pair<int,int>>>v;
+        
+     for(int i=0;i<arr.size();i++)
+     {
+        for(int j=0;j<arr.size();j++)
+        {
+            double result = static_cast<double>(arr[i]) / arr[j];
+
+            pair<int, int> second_pair = make_pair(arr[i], arr[j]);
+
+   
+            pair<double, pair<int, int>> value_to_push = make_pair(result, second_pair);
+             v.push_back(value_to_push);
+
+        }
+     }
+
+      sort(v.begin(),v.end());
+
+      return {v[k-1].second.first,v[k-1].second.second};
+
+
+
+    
+
+
+    }
+};
