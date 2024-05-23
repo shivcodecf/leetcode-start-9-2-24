623389ce2e4ed4f101c7c03fdc9c39d97be0f25f
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2024-05-23 22:44:57 +0530

    Time: 846 ms (8.27%) | Memory: 35.9 MB (90.91%) - LeetSync

---
+++ 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.cpp
@@ -0,0 +1,43 @@
+class Solution {
+   
+public: 
+    void solve(vector<int>& nums, int k, int ind, vector<int>& v, int& res) {    
+    if (ind == nums.size()) {    
+
+        if(v.size()==1)
+        {
+            res++;
+            return;
+        } 
+        int flag = 0;
+        for (int i = 0; i < v.size(); i++) {
+            for (int j = i + 1; j < v.size(); j++) {
+                if (abs(v[i] - v[j]) == k) {
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag) break;
+        }
+        if (!flag)
+            res++;
+          return;
+    }
+
+    // Include the current element in the subset
+    v.push_back(nums[ind]);
+    solve(nums, k, ind + 1, v, res);
+    v.pop_back();
+
+    // Exclude the current element from the subset
+    solve(nums, k, ind + 1, v, res);
+}
+
+int beautifulSubsets(vector<int>& nums, int k) {
+    int res = 0;
+    vector<int> v;
+    solve(nums, k, 0, v, res);
+    return res-1;
+}
+
+};
