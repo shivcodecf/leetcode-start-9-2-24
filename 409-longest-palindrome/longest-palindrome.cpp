d830319fc64fe1f28020ee584f6744af51c2872d
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2024-06-04 15:09:29 +0530

    Time: 3 ms (55.35%) | Memory: 9 MB (26.04%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.cpp
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -1,33 +1,28 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        vector<int> mp(52, 0); // Vector to store the frequency of both uppercase and lowercase letters
+        map<char,int>mp; // Vector to store the frequency of both uppercase and lowercase letters
 
         for (int i = 0; i < s.size(); i++) {
-            if (isupper(s[i])) {
-                mp[s[i] - 'A']++; // Map uppercase letters to 0-25
-            } else if (islower(s[i])) {
-                mp[s[i] - 'a' + 26]++; // Map lowercase letters to 26-51
-            }
+           mp[s[i]]++;
         }
 
         int ans = 0;
-        
-        bool hasOdd = false;
 
-        for (int i = 0; i < mp.size(); ++i) {
-            if (mp[i] % 2 == 0) {
-                ans += mp[i];
-            } else {
-                ans += mp[i] - 1; // Add the even part
-                hasOdd = true;    // Indicate that there's at least one odd count
+        int  hasOdd = 0 ;
+
+        for(auto i:mp)
+        {
+            if(i.second%2==0)
+            {
+                ans+=i.second;
+            }
+            else{
+                hasOdd=1;
+                ans+=i.second-1;
             }
         }
 
-        if (hasOdd) {
-            ans += 1; // Add one to account for a central character in the palindrome
-        }
-
-        return ans;
+        return ans+hasOdd;
     }
 };
