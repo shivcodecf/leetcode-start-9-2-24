4154d1796894aa110ec561425206059244f1c7a6
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2024-06-06 01:03:20 +0530

    Time: 29 ms (5.38%) | Memory: 18.2 MB (5.10%) - LeetSync

---
+++ 1044-find-common-characters/find-common-characters.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    vector<string> commonChars(vector<string>& words) {
+        
+
+        map<char,vector<int>>mp;
+        int n=words.size();
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<words[i].size();j++)
+            {
+                mp[words[i][j]].push_back(i);
+            }
+        }
+
+       
+        map<char,int>mp2;
+        for(auto i:mp)
+        {     
+            map<int,int>mp1;
+
+            for(int j=0;j<i.second.size();j++)
+            {
+                mp1[i.second[j]]++;
+            }
+            int mini=INT_MAX;
+
+            if(mp1.size()==n)
+            {  
+                for(auto it:mp1)
+                {
+                     mini=min(mini,it.second); 
+                }
+
+                while(mini--)
+            {
+                mp2[i.first]++;
+            }
+                
+            }
+
+            
+
+        }
+        vector<string>v;
+        
+
+        for(auto i:mp2)
+        {    
+           
+            while(i.second--)
+            {
+              v.push_back(string(1, i.first));
+            }
+           
+        }
+
+        return v;
+
+
+    }
+};
