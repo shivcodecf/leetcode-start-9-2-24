e5ff73681ef59c3bee5e5724c102ad1fb2c3485a
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2024-06-06 15:58:39 +0530

    Time: 70 ms (27.45%) | Memory: 36.5 MB (5.06%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.cpp
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -1,109 +1,124 @@
+#define pi pair<int, int>
+
 class Solution {
-public:
-    bool isNStraightHand(vector<int>& hand, int groupSize) { 
+public:  
 
-    
+    bool isNStraightHand(vector<int>& hand, int groupSize) {
 
-       map<int,int>mp;
-       int n=hand.size();
-       for(int i=0;i<n;i++)
-       {
+      
+      
+
+      map<int,int>mp;
+
+      int n=hand.size();
+
+      if(n % groupSize!=0)
+      {
+        return false;
+      }
+
+      for(int i=0;i<n;i++)
+      {
         mp[hand[i]]++;
-       } 
-       if(hand.size()%groupSize)
-       {
-        return false;
-       }
-       
-       priority_queue<pair<int,int> , vector<pair<int,int>>, greater<pair<int,int>> > pq;
-        for(auto i:mp)
+      }
+
+      priority_queue<pi, vector<pi>, greater<pi> > pq;
+
+      vector<int>ans;
+
+      vector<pair<int,int>>v; 
+
+      for(auto i:mp)
+      {
+        pq.push({i.first,i.second});
+      }
+
+      int check=0;
+
+      while(!pq.empty())
+      {  
+
+         check++;
+
+        auto it= pq.top();
+
+        int x=it.first;
+
+        int y=it.second;
+
+        ans.push_back(x);
+
+        
+
+        y--;
+
+        
+        if(y>0)
         {
-            pq.push({i.first,i.second});
-        } 
-        vector<pair<int,int>>temp;
-       
-        vector<int>v1;
-        int check=0,count=0;
+            v.push_back({x,y});
+        }
+
+        pq.pop();
 
 
-        while(pq.size())
+        if(check == groupSize)
         {  
-            
-              auto it=pq.top();
-          
-              int f=it.first;
-              int s=it.second;
-              pq.pop();
-
-            if(v1.size()==0)
-            {   
-                  
-                v1.push_back(f);
-            
-                s=s-1;
-               
-                if(s>0)
-                {    
-                    temp.push_back({f,s});
-                   
-                }
-               
-            }
-
-            else {
-                
-              if(v1.back()+1==f)
-              { 
-
-                v1.push_back(f);
-                 s=s-1; 
-                 
-                
-               
-                if(s>0)
-                {
-                   
-                     temp.push_back({f,s});
-                     
-                }
-
-              }
-              else {
-                return false;
-              }
-
-            }
-            if(v1.size()==groupSize  )
-            {
-                for(int i=0;i<temp.size();i++)
-                {
-                    pq.push({temp[i].first,temp[i].second});
-                }
-                 temp.clear();
-                 v1.clear();
-                 
-                
-                
-            }
 
 
+          for(int j=0;j<v.size();j++)
+          {
+            pq.push({v[j].first,v[j].second});
+          }
+
+           v.clear();
+           check=0;
+
+        }
 
 
-
-
-
-
-            
-        }   
-        if(v1.size())
+      }
+      int c=0;
+      if(ans.size() % groupSize!=0)
+      {
         return false;
+      }
+
+      for(int i=1;i<ans.size();i++)
+      {   
+          c++;
+
+         if(ans[i]-ans[i-1]!=1 && c<=groupSize-1)
+         {
+            return false;
+         }
+
+         if(c==groupSize)
+         {
+            c=0;
+         }
+
+         
+      }
+
+      return true;
 
 
-       
-        
-        return true;
 
-        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     }
 };
