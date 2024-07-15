a5221100ae8c2e128670f4bdbb8541c7232c493c
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2024-07-15 15:26:24 +0530

    Time: 3 ms (46.67%) | Memory: 11.7 MB (6.87%) - LeetSync

--- 726-number-of-atoms/number-of-atoms.cpp
+++ 726-number-of-atoms/number-of-atoms.cpp
@@ -1,60 +1,47 @@
 class Solution {
 public:
-    string countOfAtoms(string formula) {
-        int n = formula.length();
-        vector<int> mult(n, 1);
-        stack<int> factors;
-        int multFactor = 1;
-        for(int i=n-1; i>=0; --i) {
-            char ch = formula[i];
-            if(ch == ')') {
-                string cnt = "";
-                int k = i+1;
-                while(k<n && isdigit(formula[k])) {
-                    cnt += formula[k++];
+    string countOfAtoms(string s) {
+        int n = s.size();
+        stack<map<string, int>> st;
+        map<string, int> current;
+        
+        for (int i = 0; i < n;) {
+            if (s[i] == '(') {
+                st.push(current);
+                current.clear();
+                i++;
+            } else if (s[i] == ')') {
+                int j = i + 1;
+                while (j < n && isdigit(s[j])) j++;
+                int multiplicity = (j == i + 1) ? 1 : stoi(s.substr(i + 1, j - i - 1));
+                i = j;
+
+                if (!st.empty()) {
+                    auto temp = current;
+                    current = st.top();
+                    st.pop();
+                    for (auto& p : temp) {
+                        current[p.first] += p.second * multiplicity;
+                    }
                 }
-                if(cnt == "") cnt = "1";
-                int digit = stoi(cnt);
-                factors.push(digit);
-                multFactor *= digit;
+            } else {
+                int j = i + 1;
+                while (j < n && islower(s[j])) j++;
+                string name = s.substr(i, j - i);
+                i = j;
+                while (j < n && isdigit(s[j])) j++;
+                int multiplicity = (j == i) ? 1 : stoi(s.substr(i, j - i));
+                i = j;
+                current[name] += multiplicity;
             }
-            else if(ch == '(') {
-                int digit = factors.top();
-                factors.pop();
-                multFactor /= digit;
-            }
-            else if(isupper(ch)) {
-                mult[i] = multFactor;
-            }
-            else continue;
         }
-        map<string, int> res;
-        for(int i=0; i<n; ++i) {
-            char ch = formula[i];
-            if(!isupper(ch)) continue;
-            multFactor = mult[i];
-            string ele = "";
-            ele += ch;
-            int j = i+1;
-            while(j<n && islower(formula[j])) {
-                ele += formula[j++];
-            }
-            string cnt = "";
-            int k = j;
-            while(k<n && isdigit(formula[k])) {
-                cnt += formula[k++];
-            }
-            if(cnt == "") cnt = "1";
-            int digit = stoi(cnt);
-            res[ele] += digit * multFactor;
-            i = k-1;
+        
+        string result;
+        for (auto& p : current) {
+            result += p.first;
+            if (p.second > 1) result += to_string(p.second);
         }
-        string ans = "";
-        for(auto &[ele, digit] :res) {
-            string cnt = to_string(digit);
-            if(cnt == "1") cnt = "";
-            ans += ele + cnt;
-        }
-        return ans;
+        return result;
     }
 };
+
