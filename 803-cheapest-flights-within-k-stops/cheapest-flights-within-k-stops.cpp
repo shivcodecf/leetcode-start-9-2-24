da03b079d1fa486e357e1011c79c57a8ef138106
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2024-09-03 00:56:51 +0530

    Time: 16 ms (59.01%) | Memory: 17 MB (68.69%) - LeetSync

--- 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -1,36 +1,53 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& nums, int src, int dst, int k) {
-        vector<vector<int>> adj[n];
-        for(auto it:nums)
-        {
-            adj[it[0]].push_back({it[1],it[2]});
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<int> dis(n, 1e9); // Initialize distances to a large value
+
+        // Adjacency list for the graph
+        map<int, vector<pair<int, int>>> mp;
+
+        // Build the graph from the flights data
+        for(int i = 0; i < flights.size(); i++) {
+            mp[flights[i][0]].push_back({flights[i][1], flights[i][2]});
         }
-        queue<pair<int,pair<int,int>>> q;
-        //{stops,{node,cost}}
-        q.push({0,{src,0}});
-        vector<int> dist(n,1e9);
-        dist[src]=0;
-        while(!q.empty())
-        {
-            auto it= q.front();
-            int stops=it.first;
-            int node=it.second.first;
-            int cost=it.second.second;
-            q.pop();
-            if(stops > k) continue;
-            for(auto it:adj[node])
-            {
-                int adjNode=it[0];
-                int edW=it[1];
-                if(edW + cost < dist[adjNode] && stops<=k)
-                {
-                    dist[adjNode]=edW+cost;
-                    q.push({stops+1,{adjNode,dist[adjNode]}});
+
+        // Priority queue: {stops, {current city, cost to reach that city}}
+        priority_queue<
+            pair<int, pair<int, int>>, 
+            vector<pair<int, pair<int, int>>>, 
+            greater<pair<int, pair<int, int>>>
+        > pq;
+
+        // Start from the source city with 0 cost and 0 stops
+        pq.push({0, {src, 0}});
+        dis[src] = 0;
+
+        while(!pq.empty()) {
+            auto it = pq.top();
+            pq.pop();
+
+            int stops = it.first;
+            int city = it.second.first;
+            int cost = it.second.second;
+
+            // If we've made more than k stops, skip this path
+            if (stops > k) continue;
+
+            // Explore the neighbors of the current city
+            for(auto neighbor : mp[city]) {    
+                int nextCity = neighbor.first;
+                int flightCost = neighbor.second;
+                int newCost = cost + flightCost;
+
+                // If the new cost is less than the known distance, update and push to the queue
+                if (newCost < dis[nextCity]) {
+                    dis[nextCity] = newCost;
+                    pq.push({stops + 1, {nextCity, newCost}});
                 }
             }
         }
-        if(dist[dst]==1e9) return -1;
-        return dist[dst];
+
+        // If the destination is unreachable, return -1
+        return dis[dst] == 1e9 ? -1 : dis[dst];
     }
 };
