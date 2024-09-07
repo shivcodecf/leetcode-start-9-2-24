17e93bd98f92a851bc6270093ff08a09007cdf54
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2024-09-07 19:42:44 +0530

    Time: 59 ms (35.90%) | Memory: 12.8 MB (83.03%) - LeetSync

--- 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
+++ 1559-cherry-pickup-ii/cherry-pickup-ii.cpp
@@ -1,38 +1,77 @@
-class Solution {
-public: 
-   
-    int solve(vector<vector<int>>& grid, int i, int r1j, int r2j, vector<vector<vector<int>>>& dp) {
-        if (r1j < 0 || r2j < 0 || r1j >= grid[0].size() || r2j >= grid[0].size()) {
-            return 0;
-        }
-       
-        if (i == grid.size()) return 0;
-        if (dp[i][r1j][r2j] != -1) return dp[i][r1j][r2j];
+class Solution { 
+
+    int dp[70][70][70];
+  
+  int solve(vector<vector<int>>& grid,int x1,int y1,int y2)
+  {   
+
+          int n=grid.size();
+          int m=grid[0].size();
+
+          if( y1<0  || y1>=m ||  y2<0  || y2>=m || x1>=n) return 0;
+
+          if(dp[x1][y1][y2]!=-1) return dp[x1][y1][y2];
+
+        //   if(x1==n-1){
+
+        //     if( y1==y2)
+        //     {
+        //         return grid[x1][y1];
+        //     }
+
+        //     return grid[x1][y1] + grid[x1][y2];
+
+        //   } 
+         
+          int ans = 0;
+ 
+         
+
+         if(y1==y2 )
+         {
+           ans=grid[x1][y1];
+         }
+         else {
+            ans = grid[x1][y1] + grid[x1][y2];
+         }
+           
+           
+          int t1 = solve(grid,x1+1,y1-1,y2-1);
+
+          int t2 = solve(grid,x1+1,y1-1,y2);
+
+          int t3 = solve(grid,x1+1,y1-1,y2+1);//
+
+          int t4 = solve(grid,x1+1,y1,y2-1);//
+
+          int t5 = solve(grid,x1+1,y1,y2);//
+
+          int t6 = solve(grid,x1+1,y1,y2+1);
+
+          int t7 = solve(grid,x1+1,y1+1,y2-1);
+
+          int t8 = solve(grid,x1+1,y1+1,y2);
+
+          int t9 = solve(grid,x1+1,y1+1,y2+1);
+
+          ans+= max({t1,t2,t3,t4,t5,t6,t7,t8,t9});
+
+          return dp[x1][y1][y2]  = ans;
+
+
         
-        // Initialize ans for each call
-        int ans = 0;
-        ans += grid[i][r1j];
-        if (r1j != r2j) {
-            ans += grid[i][r2j];
-        }
-       
+  }
 
-        int local_maxi = 0; // Initialize local_maxi for each call
-
-        for (int x = r1j - 1; x <= r1j + 1; x++) {
-            for (int y = r2j - 1; y <= r2j + 1; y++) {
-                local_maxi = max(local_maxi, solve(grid, i + 1, x, y, dp));
-            }
-        }
-        ans += local_maxi;
-        return dp[i][r1j][r2j] = ans;
-    }
-
+public:
     int cherryPickup(vector<vector<int>>& grid) {
-        int n = grid.size();
-        int m = grid[0].size();
-        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(m+1, vector<int>(m+1, -1))); // Adjusted the dimensions
+         
+         int n=grid.size();
 
-        return solve(grid, 0, 0, m - 1, dp);
+         int m=grid[0].size();
+
+         memset(dp,-1,sizeof(dp));
+
+         return solve(grid,0,0,m-1);
+
     }
 };
