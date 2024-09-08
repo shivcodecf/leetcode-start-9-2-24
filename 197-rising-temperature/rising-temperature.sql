bbd5571f2253865be24f3d0540c348962715616f
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2024-09-08 16:36:30 +0530

    Time: 453 ms (48.16%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,3 +1,5 @@
+-- Select the IDs from Weather where the temperature is higher than the previous day
 SELECT w1.id
 FROM Weather w1, Weather w2
-WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1 AND w1.temperature > w2.temperature;
+WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1
+  AND w1.temperature > w2.temperature;
