26c2e7fa7d5ce140fa4fb0de989980dd9a4adb2d
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2024-09-16 02:44:13 +0530

    Time: 23 ms (55.77%) | Memory: 27.9 MB (11.81%) - LeetSync

--- 503-next-greater-element-ii/next-greater-element-ii.cpp
+++ 503-next-greater-element-ii/next-greater-element-ii.cpp
@@ -1,52 +1,57 @@
 class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
-          
-          vector<int>v;
-          int n=nums.size();
-
-    //   for(int i=0;i<nums.size();i++)
-    //   {
-    //     v.push_back(nums[i]);
-    //   } 
-
+        
         stack<int>st;
 
-    //    for(int i=0;i<nums.size();i++)
-    //   {  
-        
-    //     v.push_back(nums[i]);
-    //   }
-      
-       for(int i=n-1;i>=0;i--)
-      {  
-        st.push(nums[i]);
-        
-      }
-      
-     
-       vector<int>ans;
+        for(int i=nums.size()-1;i>=0;i--)
+        {
+            st.push(nums[i]);
+        }
 
-      for(int i=n-1;i>=0;i--)
-      {
-         
-         while(st.size() && st.top()<=nums[i])
-         {
-            st.pop();
-         }
-         if(st.empty())
-         {
-            ans.push_back(-1);
-         }
-         else {
-            ans.push_back(st.top());
-         }
-         st.push(nums[i]);
+        vector<int>ans;
 
-      }
-      reverse(ans.begin(),ans.end());
-      return ans;
-      
+        for(int i=nums.size()-1;i>=0;i--)
+        {     
+              int flag=0;
+              if(st.empty())
+              {
+                ans.push_back(-1);
+                continue;
+              }
+
+              while(!st.empty())
+              {
+                int x=st.top();
+                if(nums[i]<x)
+                {    
+                    flag=1;
+                    ans.push_back(x);
+                    break;
+                }
+                st.pop();
+                
+              }
+
+              st.push(nums[i]);
+
+              if(!flag)
+              {
+                ans.push_back(-1);
+              }
+        }
+
+        reverse(ans.begin(),ans.end());
+
+        return ans;
+
+
+
+
+
+
+
+
 
     }
 };
