7314d604cbac0952ad786dac7a74f25514959885
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2024-09-18 01:33:24 +0530

    Time: 14 ms (27.25%) | Memory: 10.3 MB (57.78%) - LeetSync

--- 402-remove-k-digits/remove-k-digits.cpp
+++ 402-remove-k-digits/remove-k-digits.cpp
@@ -7,29 +7,68 @@ public:
         stack<char>st;
         string ans="";
         for(int i=0;i<n;i++)
-        {
+        {   
+
             while(!st.empty() && k>0 && st.top()>num[i])
             {
                 st.pop();
+
                 k--;
             }
 
             st.push(num[i]);
 
-            if(st.size()==1&&st.top()=='0')st.pop();
+            // if(st.size()==1&&st.top()=='0')st.pop();
         }
-        while(k&&!st.empty())
-        {
+
+        while(k  && !st.empty())
+        { 
+
             k--;
+
             st.pop();
+
         }
         while(!st.empty())
-        {
+        {  
+
             ans+=st.top();
+
             st.pop();
+
         }
-        if(ans=="")return "0";
-        else reverse(ans.begin(),ans.end());
-        return ans;
+
+        string res="";
+        int flag=0;
+
+        int m=ans.size(),ind = -1;
+
+        for(int i=ans.size()-1;i>=0;i--)
+        {  
+
+          if(ans[i]=='0' && flag==0)
+          {
+            continue;
+          }
+
+          else {
+          
+          ind = i;
+          break;
+            
+
+          }
+
+
+        }
+
+        for(int i=0;i<=ind;i++)
+        {
+            res+=ans[i];
+        }
+
+        if(res=="")return "0";
+        else reverse(res.begin(),res.end());
+        return res;
     }
 };
