c62941b8f1bf680dc232e8759eec7972e1325eee
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2024-09-18 14:06:26 +0530

    Time: 8 ms (48.02%) | Memory: 15.5 MB (94.62%) - LeetSync

--- 179-largest-number/largest-number.cpp
+++ 179-largest-number/largest-number.cpp
@@ -15,14 +15,33 @@ public:
         sort(nums.begin(), nums.end(), custom);
 
         // If the largest number is "0", return "0" (to avoid multiple leading zeros)
-        if (nums[0] == 0) return "0";
+        // if (nums[0] == 0) return "0";
 
         // Build the resulting string by concatenating the numbers
         string ans = "";
-        for (int num : nums) {
-            ans += to_string(num);
-        }
+        int flag=0;
 
+        for (int num : nums) {
+
+            if(num==0 && flag==0)
+            {
+                continue;
+            }
+            else {
+               ans += to_string(num);
+               flag=1;
+             }
+
+
+            }
+
+            
+
+        
+        if(ans=="")
+        {
+            return "0";
+        }
         return ans;
     }
 };
