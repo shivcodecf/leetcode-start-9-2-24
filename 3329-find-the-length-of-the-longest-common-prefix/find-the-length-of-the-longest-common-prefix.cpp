d8c17354e713548ca80920051b1c9d4aac72f533
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2024-09-24 12:10:33 +0530

    Time: 1342 ms (5.11%) | Memory: 165.2 MB (50.93%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
@@ -3,14 +3,14 @@ public:
     int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
 
 
-        map<string, bool> vis; 
+        map<string, int> vis; 
 
         for(int j=0;j<arr1.size();j++){
 
             string s = to_string(arr1[j]); string cc = ""; 
             for(int i = 0; i < s.size(); i++){
 
-                cc += s[i]; vis[cc] = true; 
+                cc += s[i]; vis[cc]++;
             }
         }
 
@@ -23,11 +23,14 @@ public:
 
             for(int i = 0; i < s.size(); i++){
 
-                cc += s[i]; if(vis[cc] == true)ans = max(ans, (i + 1)); 
+                cc += s[i];
+
+                 if(vis.find(cc)!=vis.end()) 
+                 ans = max(ans, (i + 1)); 
 
             }
         }
-        
+
         return ans; 
 
 
