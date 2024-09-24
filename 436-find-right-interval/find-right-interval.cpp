4965737e3e46f4a9ce0d5e7f4c75d68f58667b11
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2024-09-24 20:29:48 +0530

    Time: 44 ms (79.41%) | Memory: 28.1 MB (77.72%) - LeetSync

--- 436-find-right-interval/find-right-interval.cpp
+++ 436-find-right-interval/find-right-interval.cpp
@@ -15,7 +15,7 @@ public:
         
         // For each interval, find the minimum start that is >= end using binary search
         for (int i = 0; i < n; ++i) {
-            
+
             int end = intervals[i][1];
             int low = 0, high = n - 1;
             int bestIdx = -1;
@@ -23,19 +23,16 @@ public:
             // Binary search to find the smallest start >= end
             while (low <= high) {
 
+
                 int mid = low + (high - low) / 2;
-
                 if (starts[mid].first >= end) {
-
                     bestIdx = starts[mid].second;  // Store the index of the valid interval
                     high = mid - 1;  // Try to find a smaller start
-
                 } else {
-
                     low = mid + 1;
-
                 }
 
+                
             }
             
             ans[i] = bestIdx;  // Store the index of the right interval, or -1 if not found
