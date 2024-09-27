55e39824add483b15f4b980c0505d302f00c53c8
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2024-09-28 00:28:09 +0530

    Time: 236 ms (81.64%) | Memory: 160.8 MB (58.92%) - LeetSync

--- 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
+++ 3329-find-the-length-of-the-longest-common-prefix/find-the-length-of-the-longest-common-prefix.cpp
@@ -1,38 +1,95 @@
+struct TrieNode
+{
+    TrieNode* children[10]; //0,1,2,3,4,5,6,7,8,9
+};
+
 class Solution {
 public:
-    int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
 
+  TrieNode* getTrieNode(){
 
-        map<string, int> vis; 
+    TrieNode* node = new TrieNode();
 
-        for(int j=0;j<arr1.size();j++){
+    for(int i=0;i<10;i++)
+    {
+        node->children[i] = NULL;
+    }
 
-            string s = to_string(arr1[j]); string cc = ""; 
-            for(int i = 0; i < s.size(); i++){
+    return node;
 
-                cc += s[i]; vis[cc]++;
-            }
-        }
+  }
 
+  void insert(int num,TrieNode* root)
+  {
+    TrieNode* crawl = root;
 
-        int  ans = 0; 
+    string s = to_string(num);
 
-        for(int j=0;j<arr2.size();j++){
+    for(char ch:s)
+    { 
 
-            string s = to_string(arr2[j]); string cc = "";
+       int idx = ch-'0';
 
-            for(int i = 0; i < s.size(); i++){
+       if(crawl->children[idx]==NULL)
+       {
+        crawl ->children[idx] = getTrieNode();
+       }
 
-                cc += s[i];
-
-                 if(vis.find(cc)!=vis.end()) 
-                 ans = max(ans, (i + 1)); 
-
-            }
-        }
-
-        return ans; 
-
+       crawl = crawl->children[idx];
 
     }
+
+  }
+  // return len of longest prefix
+  int search(int num,TrieNode* root)
+  {
+       TrieNode*crawl = root;
+
+       string s = to_string(num);
+
+       int res=0;
+
+       for(char ch:s)
+       {
+        int idx = ch-'0';
+        if(crawl->children[idx])
+        {   
+            res++;
+            crawl = crawl->children[idx];
+        }
+        else {
+            break;
+        }
+       } 
+
+       return res;
+  }
+
+
+
+
+
+    int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
+        
+        TrieNode* root = getTrieNode();
+
+        for(int num : arr1)
+        {
+            insert(num,root);
+        }
+
+        int result =0;
+
+        for(int num : arr2)
+        {
+            result = max(search(num,root),result);
+        }
+
+        return result;
+
+
+        
+    }
+
+
 };
