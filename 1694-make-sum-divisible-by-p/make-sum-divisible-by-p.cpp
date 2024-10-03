4e1cb800e956872aca84dbd38171dd71db5a0834
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2024-10-04 00:31:04 +0530

    Time: 217 ms (15.94%) | Memory: 74.9 MB (8.48%) - LeetSync

--- 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.cpp
+++ 1694-make-sum-divisible-by-p/make-sum-divisible-by-p.cpp
@@ -9,20 +9,20 @@ public:
 
       pref[0] = nums[0];
 
-      int sum = nums[0];
+      long long sum = nums[0];
 
      for(int i=1;i<n;i++)
      { 
 
         pref[i] = (pref[i-1] + nums[i])%p;
 
-        sum = (sum+nums[i])%p;
+        sum = (sum+nums[i]);
 
      }
 
         map<int,int>mp;
 
-       int target = sum%p, ans = INT_MAX;
+       long long target = sum%p, ans = INT_MAX;
 
        if(target==0)
        { 
@@ -32,19 +32,19 @@ public:
        }
 
        mp[0] = -1;
-       int curr =0;
+       long long curr =0;
 
      for(int i=0;i<n;i++)
      {   
 
            curr = (curr+nums[i])%p;
            
-          int prev = (curr - target+p)%p;
+          long long prev = (curr - target+p)%p;
 
           if(mp.find(prev)!=mp.end())
           { 
 
-            ans = min(ans,i-mp[prev]);
+            ans = min(ans,(long long)i-mp[prev]);
 
 
           }
