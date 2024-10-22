f7610df5d7dba3034d246b356dc4f8d8516b145c
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2024-10-22 10:41:06 +0530

    Time: 221 ms (63.69%) | Memory: 47.8 MB (82.42%) - LeetSync

--- 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.cpp
+++ 1715-split-a-string-into-the-max-number-of-unique-substrings/split-a-string-into-the-max-number-of-unique-substrings.cpp
@@ -1,23 +1,23 @@
-class Solution {
-    void solve(string& s1, int idx, int& ans, int& temp_ans, unordered_set<string>& seen) { 
+int ans;
 
+class Solution {
+    void solve(string& s1, int idx, int& temp_ans, unordered_set<string>& seen) { 
         if (idx >= s1.size()) {
             ans = max(ans, temp_ans); // Update the answer if the current split is larger
             return;
         }
 
-
         for (int i = 1; i <= s1.size() - idx; i++) {
             string s2 = s1.substr(idx, i); // Generate substring
             
-            if (seen.find(s2) == seen.end()) { // Check if it's a unique substring
-                seen.insert(s2); // Mark it as seen
-                temp_ans++; // Increment the count of unique substrings
+            if (seen.find(s2) == seen.end()) { 
+                seen.insert(s2); 
+                temp_ans++; 
 
-                // Recur for the next part of the string
-                solve(s1, idx + i, ans, temp_ans, seen);
+              
+                solve(s1, idx + i,  temp_ans, seen);
 
-                // Backtrack: undo changes
+                
                 temp_ans--;
                 seen.erase(s2);
             }
@@ -26,14 +26,12 @@ class Solution {
 
 public:
     int maxUniqueSplit(string s) {
-
-        int ans = 0;
+         ans = 0;
         int temp_ans = 0;
         unordered_set<string> seen; // Use set for uniqueness check
         
-        solve(s, 0, ans, temp_ans, seen); // Start recursion
+        solve(s, 0, temp_ans, seen); // Start recursion
         
         return ans;
-        
     }
 };
