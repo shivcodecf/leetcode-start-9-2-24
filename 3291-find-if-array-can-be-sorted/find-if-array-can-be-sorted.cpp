ddc84b4256a4cc7b15c4c4eb6b4f8a7956f89656
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2024-11-07 00:29:00 +0530

    Time: 0 ms (100.00%) | Memory: 31.1 MB (86.44%) - LeetSync

---
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    bool canSortArray(vector<int>& nums) {
+
+        int ans = 0;
+
+        for (int i = 0; i < nums.size() - 1; i++) {
+
+            bool flag=false;
+
+            for (int j = 0; j < nums.size() - i - 1;j ++) {
+
+                if (nums[j + 1] < nums[j]) {
+
+                     flag = true;
+
+                    int count = __builtin_popcount(nums[j]);
+                      
+                    int count1 = __builtin_popcount(nums[j + 1]);
+
+                    if (count == count1) {
+                        swap(nums[j], nums[j + 1]);
+                    }
+                    else {
+                        return false;
+                    }
+                   
+                }
+            }
+
+            if(!flag)
+            {
+                return true;
+            }
+        }
+
+        return true;
+    }
+};
