004e5bb774bd0e22d97a6c906bda1d19d0b01410
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2024-11-12 18:57:58 +0530

    Time: 27 ms (100.00%) | Memory: 101.3 MB (25.00%) - LeetSync

--- 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.cpp
+++ 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.cpp
@@ -1,83 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+
 class Solution {
 public:
-    vector<int> maximumBeauty(vector<vector<int>>& items, vector<int>& queries) {
-
-
-
-        // auto comparator = [](const pair<int,int>& a,pair<int,int>& b)
-        // {
-        //     if(a.first==b.first)
-        //     {
-        //         return a.second < b.second;
-        //     }
-        //     return a.first < b.first;
-        // };
-        
-     vector<pair<int,int>>v;
-
-     for(int i=0;i<items.size();i++)
-     {  
-        v.push_back({items[i][0],items[i][1]});
-     }
-
-     sort(v.begin(),v.end());
-
-     vector<pair<int,pair<int,int>>>v1;
-
-     int maxi = -1;
-
-     for(int i=0;i<items.size();i++)
-     {    
-         maxi = max(maxi,v[i].second);
-        v1.push_back({v[i].first,{v[i].second,maxi}});
-     }
-
-
-
-
-     
-
-     int n=v.size();
-
-     vector<int>ans;
-
-     for(int i=0;i<queries.size();i++)
-     {
-        int s = 0;
-        int e = n-1;
-
-        int value = queries[i],an=INT_MIN;
-
-        while(s<=e)
-        {
-            int mid = s+(e-s)/2;
-
-            if(v1[mid].first<=value)
-            {   
-                an = max(an,v1[mid].second.second);
-                s = mid+1;
-            }
-            else {
-              e = mid-1;
-            }
-
-            
-        }
-         
-         if(an!=INT_MIN)
-        ans.push_back(an);
-        else {
-            ans.push_back(0);
+    std::vector<int> maximumBeauty(std::vector<std::vector<int>>& items, std::vector<int>& queries) {
+        std::vector<std::pair<int, int>> v;
+        for (const auto& item : items) {
+            v.push_back({item[0], item[1]});
         }
 
+        // Sort items by price
+        std::sort(v.begin(), v.end());
 
-     }
+        // Precompute maximum beauty up to each price
+        std::vector<std::pair<int, int>> maxBeautyByPrice;
+        int maxBeauty = -1;
+        for (const auto& [price, beauty] : v) {
+            maxBeauty = std::max(maxBeauty, beauty);
+            maxBeautyByPrice.push_back({price, maxBeauty});
+        }
 
-     return ans;
+        std::vector<int> ans;
+        for (const int query : queries) {
+            // Use upper_bound to find the first element with price > query
+            auto it = std::upper_bound(maxBeautyByPrice.begin(), maxBeautyByPrice.end(), std::make_pair(query, INT_MAX));
 
+            if (it == maxBeautyByPrice.begin()) {
+                ans.push_back(0);  // No items affordable
+            } else {
+                --it;  // Go to the last affordable item within budget
+                ans.push_back(it->second);
+            }
+        }
 
-
-
-
+        return ans;
     }
 };
