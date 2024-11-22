0f6af4ece88160d42244652b899c71f5d87eb628
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2024-11-22 21:47:15 +0530

    Time: 5 ms (39.09%) | Memory: 131 MB (17.22%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -11,55 +11,32 @@
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-
-     ListNode* temp= head;
-
-     string s1="";
-     int len=0;
-     vector<int>v,v1;
-
-     while(temp!=nullptr)
-     {
-        v.push_back(temp->val);
-        temp=temp->next;
-        len++;
-     } 
-
-        ListNode* node = nullptr;
-
-        while (head != nullptr) {
-              ListNode* temp = head->next;
-            
-           
-             head->next = node;
-           
-            
-             node = head;
-             head = temp;
-
-        }
-
-        ListNode*temp1= node;
-
-        while(temp1!=nullptr)
-        {
-            v1.push_back(temp1->val);
-            temp1 = temp1->next;
-        }
-         
-         for(int i=0;i<len;i++)
-         {
-            if(v[i]!=v1[i])
-            {
-                return false;
-            }
-         }
-
-         return true;
         
 
 
+        vector<int>v;
 
+        while(head!=nullptr)
+        {
+         v.push_back(head->val);
+         head = head->next;
+        }
+
+      int i=0,j=v.size()-1;
+
+      while(i<=j)
+      {
+        if(v[i]!=v[j])
+        {
+            return false;
+        }
+        i++;
+        j--;
+      }
+
+      return true;
+
+       
 
 
     }
