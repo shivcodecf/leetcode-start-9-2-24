708121715ea6d03c80eb133646af7b8cdcf95506
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2024-11-23 00:36:41 +0530

    Time: 0 ms (100.00%) | Memory: 25.9 MB (30.82%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,28 +1,96 @@
 class Solution {
 public:
     int trap(vector<int>& height) {
-        if(height.empty()) 
-            return 0; 
         
-        int res = 0;
-        stack<int> s;   
-        for(int i=0; i<height.size(); i++) {
-            while(!s.empty() && height[i] > height[s.top()]) {
-                int rightIdx = s.top();
-                s.pop();
-                
-                if(s.empty()) 
-                    break;
-                
-                int leftIdx = s.top();
-                int length = min(height[i], height[leftIdx]) - height[rightIdx];
-                int breadth = i - leftIdx - 1;
-                int area= length*breadth;
-                if(area>0)
-                res += length*breadth;
-            }
-            s.push(i);
+        int n = height.size();
+
+        vector<int>v1(n),v2(n);
+
+        int maxi=-1;
+
+        for(int i=0;i<n;i++)
+        {
+          maxi = max(maxi,height[i]);
+          v1[i]=(maxi);
         }
-        return res;
+
+        maxi=-1;
+
+          for(int i=n-1;i>=0;i--)
+        {
+          maxi = max(maxi,height[i]);
+          v2[i]=(maxi);
+        }
+
+        int ans=0;
+
+        // reverse(v2.begin(),v2.end());
+
+        for(int i=1;i<n-1;i++)
+        {
+           
+        int x = v1[i-1];
+
+        int y = v2[i+1];
+
+        if(x>height[i] && y>height[i])
+        {
+            int mini = min(x,y);
+
+            int left = mini-height[i];
+
+            ans+=left;
+        }
+
+        
+
+
+
+        }
+
+        return ans;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
     }
-    };
+};
