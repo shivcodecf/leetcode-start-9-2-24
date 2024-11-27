24f0f791c9748495e4042f0612667efda1910efa
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2024-11-28 00:35:58 +0530

    Time: 841 ms (98.65%) | Memory: 0B (100.00%) - LeetSync

--- 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -18,7 +18,7 @@
 
 Select 
 contest_id, 
-round(count(distinct user_id) * 100 /(select count(user_id) from Users) ,2) as percentage
+round(count(distinct user_id)  /(select count(user_id) from Users)*100 ,2) as percentage
 from  Register
 group by contest_id
 order by percentage desc,contest_id
