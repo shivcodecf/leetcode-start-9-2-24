298f186edd409195531bbb71166ac4828936d99e
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2024-12-07 18:21:40 +0530

    Time: 6 ms (80.78%) | Memory: 22.9 MB (15.78%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,60 +1,45 @@
-#define ll long long
 class Solution {
-public:  
-       
-    ll solve(vector<int>& piles,ll ind,ll h,ll mid)
-    {   
-        ll hour=0,sum=0;
-        for(int i=0;i<piles.size();i++)
-        {  
-          ll d = piles[i]/mid;
-          ll r= piles[i]%mid;
-          if(mid>=piles[i])
-          {
-            hour++;
-          }
-          else {
-             hour+=d;
-             if(r!=0)
-             {
-                hour++;
-             }
-          }
-        }
+   
+   int solve(int mid,vector<int>& piles,int h)
+   { 
 
-        return hour;
+    long long sum=0;
+
+    for(int i=0;i<piles.size();i++)
+    {
+      sum+=(piles[i]+mid-1)/mid;
+
     }
 
+    return sum<=h;
 
+   }
+
+public:
     int minEatingSpeed(vector<int>& piles, int h) {
         
-    ll low = 1;
+        int high = *max_element(piles.begin(),piles.end());
 
-    ll high = *max_element(piles.begin(),piles.end());
+        int low = 1,ans = INT_MAX;
 
-    ll mid=0,ans=0;
-
-    while(low<=high)
-    {
-        mid = low+(high-low)/2;
-      
-
-        ll res = solve(piles,0,h,mid);
-
-        if(res>h)
+        while(low<=high)
         {
-          low = mid+1;
+            int mid = low+(high-low)/2;
+
+            if(solve(mid,piles,h))
+            {  
+                ans = min(ans,mid);
+                high = mid-1;
+               
+            }
+            else {
+                low = mid+1;
+            }
+
 
         }
-        else {
-            ans = mid;
-            high = mid-1;
-         
-        }
 
-    }
-
-    return ans;
+        return ans;
 
     }
 };
