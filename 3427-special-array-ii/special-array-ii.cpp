3b4f35213ffef6c2e8971f48d205efb3269bcf61
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2024-12-09 13:00:29 +0530

    Time: 0 ms (100.00%) | Memory: 126.7 MB (63.34%) - LeetSync

--- 3427-special-array-ii/special-array-ii.cpp
+++ 3427-special-array-ii/special-array-ii.cpp
@@ -1,72 +1,48 @@
-
 class Solution {
 public:
     vector<bool> isArraySpecial(vector<int>& nums, vector<vector<int>>& queries) {
+        
+        int n=nums.size();
 
+        vector<int>pref(n,0);
 
-     int n=nums.size();
-     int m=queries.size();
-     vector<bool>ans;
+        pref[0] = 0;
 
-     if(n==1)
-     {   
-
-        while(m--)
+        for(int i=1;i<n;i++)
         {
-           ans.push_back(true);
+           if(nums[i-1]%2!=nums[i]%2)
+           {
+            pref[i] = pref[i-1]+1;
+           }
+           else {
+            pref[i] = 0;
+           }
         }
-       
 
-        return ans;
-     }
+        int m=queries.size();
 
-     vector<int>v;
-     int c=0;
-       v.push_back(0);
-     for(int i = 0;i<n-1;i++)
-     { 
-       
-      if(nums[i]%2==nums[i+1]%2)
-      {
-        c++;
-        v.push_back(c);
-        
-      }
-      else {
-        v.push_back(c);
-      }
+        vector<bool>v1;
 
-     }
-   
-     
-
-       
-     
-
-     for(int i=0;i<m;i++)
-     {
-
-        int x= queries[i][0];
-        int y=queries[i][1];
-        
-        
-       
-        
-
-        int diff=v[y]-v[x];
-
-        if(diff<=0)
+        for(int i=0;i<m;i++)
         {
-         ans.push_back(true);
+            int x = queries[i][0];
+
+            int y = queries[i][1];
+
+            if(pref[y]-pref[x]==y-x)
+            {
+             v1.push_back(true);
+            }
+            else {
+                v1.push_back(false);
+            }
+
+
         }
-        else {
-            ans.push_back(false);
-        }
+
+        return v1;
 
 
-     }
-
-     return ans;
 
 
 
