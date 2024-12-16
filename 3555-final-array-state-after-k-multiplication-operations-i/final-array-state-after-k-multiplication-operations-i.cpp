b28652c786594b829b11deda2efe6875e63a78b1
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2024-12-17 00:59:54 +0530

    Time: 1 ms (40.45%) | Memory: 28.6 MB (5.01%) - LeetSync

--- 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.cpp
+++ 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.cpp
@@ -1,57 +1,76 @@
-#include <iostream>
-#include <vector>
-#include <queue>
-#include <algorithm>
-
-using namespace std;
-
 class Solution {
 public:
     vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
         
-        // Custom comparator
-        auto comparator = [](const pair<int, int>& a, const pair<int, int>& b) {
-            if (a.first == b.first) {
-                return a.second > b.second;  // Min-heap by index if priorities are equal
+        auto com = [](const pair<int,int>&a ,const pair<int,int>& b)
+        {
+            if(a.first==b.first)
+            {
+                return a.second < b.second;
             }
-            return a.first > b.first;  // Min-heap by priority
+
+            return a.first < b.first;
         };
+
+        priority_queue<pair<int,int>,vector<pair<int,int>>, greater<pair<int,int>>>pq;
+
+        for(int i=0;i<nums.size();i++)
+        {
+            pq.push({nums[i],i});
+        }
+
+        while(!pq.empty())
+        { 
+
+          auto it = pq.top();
+
+          pq.pop();
+
+          int x = it.first;
+
+          int y = it.second;
+
+          pq.push({x*multiplier,y});
+
+          k--;
+
+          if(k==0)
+          {
+            break;
+          }
+
+
+
+
+        }
+
         
-        // Priority queue with custom comparator
-        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(comparator)> pq(comparator);
 
-        // Push initial pairs of (value, index) into the priority queue
-        for (int i = 0; i < nums.size(); i++) {
-            pq.push({nums[i], i});
-        }
+         vector<pair<int,int>>v1;
 
-        // Process k operations
-        while (k--) {
-            int x = pq.top().first;
-            int y = pq.top().second;
+        while(!pq.empty())
+        {  
+            auto it = pq.top();
             pq.pop();
+            v1.push_back({it.second,it.first});
 
-            long long z = x * multiplier;
-
-            pq.push({z, y});
         }
 
-        // Collect the results in a vector of pairs (index, value)
-        vector<pair<int, int>> ans;
-        while (!pq.empty()) {
-            ans.push_back({pq.top().second, pq.top().first});
-            pq.pop();
+        sort(v1.begin(),v1.end());
+
+        vector<int>ans;
+
+       
+
+        for(int i=0;i<v1.size();i++)
+        {
+            ans.push_back(v1[i].second);
         }
 
-        // Sort the result by index to maintain the original order
-        sort(ans.begin(), ans.end());
+        return ans;
 
-        // Extract the final values from the sorted pairs
-        vector<int> res;
-        for (const auto& p : ans) {
-            res.push_back(p.second);
-        }
 
-        return res;
+
+
     }
 };
