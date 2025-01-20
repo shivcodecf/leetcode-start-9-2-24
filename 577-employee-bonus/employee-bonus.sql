daa1edcde3edf84efa100d8d4c032c21860fec55
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2025-01-21 01:47:13 +0530

    Time: 1158 ms (65.89%) | Memory: 0B (100.00%) - LeetSync

--- 577-employee-bonus/employee-bonus.sql
+++ 577-employee-bonus/employee-bonus.sql
@@ -1,5 +1,24 @@
-SELECT e.name, b.bonus
-FROM Employee e
-left JOIN Bonus b
-ON e.empId = b.empId 
-WHERE b.bonus < 1000 or b.bonus is null;
+# Write your MySQL query statement below
+select e.name,b.bonus from Employee e
+
+LEFT JOIN
+
+Bonus b
+
+ON
+
+e.empId = b.empId 
+
+
+
+
+
+where
+
+b.bonus<1000 
+
+OR
+
+b.empId is NULL;
+
+
