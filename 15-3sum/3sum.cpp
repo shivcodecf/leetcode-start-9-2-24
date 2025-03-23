777124d84d75f6ca1a8ca373d70437e632bed23a
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2025-03-23 12:25:31 +0530

    Time: 44 ms (81.02%) | Memory: 29 MB (68.41%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -8,32 +8,36 @@ public:
         sort(nums.begin(), nums.end());
 
         for (int i = 0; i < n - 2; i++) {
-            // Skip duplicates for the first number
+           
             if (i > 0 && nums[i] == nums[i - 1]) continue;
 
             int target = -nums[i];
             int left = i + 1, right = n - 1;
 
             while (left < right) {
+
                 int sum = nums[left] + nums[right];
 
                 if (sum == target) {
+
                     ans.push_back({nums[i], nums[left], nums[right]});
 
-                    // Skip duplicates for the second number
+                   
                     while (left+1 < right && nums[left] == nums[left + 1]) left++;
-                    // Skip duplicates for the third number
+                   
                     while (left < right-1 && nums[right] == nums[right - 1]) right--;
 
-                    // Move pointers
+                    
                     left++;
                     right--;
+
 
                 } else if (sum < target) {
                     left++;
                 } else {
                     right--;
                 }
+
             }
         }
 
