e08f8e748cc19ea4e9fcf1e4ee6231f9076e9c9c
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2025-07-17 16:47:13 +0530

    Time: 36 ms (14.61%) | Memory: 35.9 MB (66.49%) - LeetSync

--- 542-01-matrix/01-matrix.cpp
+++ 542-01-matrix/01-matrix.cpp
@@ -1,79 +1,56 @@
 class Solution {
 public:
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-      
-        int n = mat.size(),m = mat[0].size();
 
-        queue<pair<int,pair<int,int>>>q;
+        int n = mat.size(), m = mat[0].size();
 
-         vector<vector<int>>v(n,vector<int>(m,INT_MAX));
+        queue<pair<int, pair<int, int>>> q;
 
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-                if(mat[i][j]==0)
+        vector<vector<int>> v(n, vector<int>(m, INT_MAX));
 
-                {   
-                     v[i][j] = 0;
-                    q.push({0,{i,j}});
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (mat[i][j] == 0)
+
+                {
+                    v[i][j] = 0;
+                    q.push({0, {i, j}});
                 }
             }
         }
 
-       
-        
+        int drow[] = {-1, 0, +1, 0};
 
-        int drow [] ={-1,0,+1,0};
+        int dcol[] = {0, +1, 0, -1};
 
-         int dcol [] ={0,+1,0,-1};
+        while (!q.empty()) {
 
+            auto it = q.front();
+            int x = it.first;
 
-        while(!q.empty())
-        {
-             
-             auto it = q.front();
-           int x = it.first;
+            int y = it.second.first;
 
-           int y = it.second.first;
+            int z = it.second.second;
 
-           int z = it.second.second;
+            q.pop();
 
-           q.pop();
+            for (int k = 0; k < 4; k++) {
 
-           for(int k=0;k<4;k++)
-           {  
+                int nrow = drow[k] + y;
 
-            int nrow = drow[k] + y;
+                int ncol = dcol[k] + z;
 
-            int ncol = dcol[k] + z;
+                if (nrow >= 0 && ncol >= 0 && nrow < n && ncol < m &&
+                    x + 1 < v[nrow][ncol]) {
 
-            if(nrow>=0 && ncol>=0 && nrow<n && ncol<m && x+1 < v[nrow][ncol])
-            {   
+                    v[nrow][ncol] = x + 1;
 
-                v[nrow][ncol] = x+1;
-
-                q.push({x+1,{nrow,ncol}});
-
-
+                    q.push({x + 1, {nrow, ncol}});
+                }
             }
-
-
-
-
-
-           }
-
-
-
-
+            
         }
 
         return v;
-
-       
-
-
-
     }
 };
