fa010af78ecaa099f93248a55b297743c06a36cc
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2025-08-13 03:55:46 +0530

    Time: 27 ms (32.50%) | Memory: 70.7 MB (14.75%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,61 +1,86 @@
 class Solution {
-public: 
-      
-      int solve(vector<int>& bloomDay,int ban,int adj,int mid)
-      {
-          int cnt=0,total=0;
-         for(int i=0;i<bloomDay.size();i++)
-         {
-             if(mid>=bloomDay[i])
-             {   
 
-                cnt++;
-              if(cnt==adj)
-              {
-                  total++;
-                  cnt=0;
-              }
+       bool check(vector<int>& bloomDay,int target,int k,int m)
+       { 
 
+          int n = bloomDay.size();
 
+          map<int,int>mp;
+
+          int check = 0,sum=0;
+
+          // [1,10,3,10,2]
+
+          for(int i=0;i<n;i++)
+          { 
+
+             if(bloomDay[i]<=target)
+             {
+                check++;
              }
-             else {
-               cnt=0;
+             else{
+                check=0;
              }
+
+             if(check==k)
+             {
+                sum++;
+                check=0;
+             }
+
+             if(sum == m)
+             {
+                return true;
+             }
+
+          }
+
+          return false;
+
+
+       }
+
+public:
+    int minDays(vector<int>& bloomDay, int m, int k) {
+         
+         int n = bloomDay.size();
+
+
+         int high = *max_element(bloomDay.begin(),bloomDay.end());
+
+         
+
+         int low = *min_element(bloomDay.begin(),bloomDay.end());
+
+
+         cout <<high <<" "<<low<<endl;
+
+         int ans = INT_MAX;
+
+
+         while(low<=high)
+         {  
+
+            int mid = low+(high-low)/2;
+
+            if(check(bloomDay,mid,k,m))
+            {
+                high = mid-1;
+                ans = min(ans,mid);
+            }
+            else {
+                low = mid+1;
+            }
+
+
          }
-         return total;
 
-      }
+         if(ans == INT_MAX)
+         {
+            return -1;
+         }
 
-
-
-     int minDays(vector<int>& bloomDay, int m, int k) {
-        
-
-     int maxi = *max_element(bloomDay.begin(),bloomDay.end());
-
-     int mini=1,ans=INT_MAX,mid=0;
-
-    while(mini<=maxi)
-    {
-        mid= mini+(maxi-mini)/2;
-        if(solve(bloomDay,m,k,mid)>=m){
-            ans=min(ans,mid);
-            maxi=mid-1;
-
-        }
-        else {
-            mini=mid+1;
-        }
-        
-
-
-
-
-    }
-    if(ans!=INT_MAX)
-    return ans;
-    return -1;
-      
+         return ans;
 
 
     }
