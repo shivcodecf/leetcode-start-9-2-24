5ac89c60bd5f489bea4da0262daeb796247d7ab9
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2025-08-12 22:49:37 +0530

    Time: 67 ms (38.17%) | Memory: 49.1 MB (26.61%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,22 +1,58 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int> mp;
-        int prefixSum = 0, ans = 0;
 
-        mp[0] = 1; // Edge case for when prefixSum itself is k
+        int ans = 0;
 
-        for (int num : nums) {
-            
-            prefixSum += num;
-            int check = prefixSum - k;
+        map<int, int> mp;
 
-            if (mp.find(check) != mp.end()) {
-                ans += mp[check]; // Add all previous occurrences
-            }
+        int n = nums.size();
 
-            mp[prefixSum]++; // Store the frequency of prefixSum
+        vector<int> pref(n, 0);
+
+        pref[0] = nums[0];
+
+        // [1,-1,0] --->  [1,0,0]   k = 0;
+
+       
+        for (int i = 1; i < nums.size(); i++) {
+            pref[i] = pref[i - 1] + nums[i];
+
+           
         }
+
+        for (int i = 0; i < pref.size(); i++) {
+
+            if (pref[i] == k) {
+
+                ans++;
+
+            }
+           
+
+                int left = pref[i] - k;
+
+                if (mp.find(left) != mp.end()) {
+
+                    ans+=mp[left];
+
+                   
+
+                    if (mp[left] == 0) {
+                        mp.erase(left);
+                    }
+
+                }
+            
+
+              mp[pref[i]]++;
+
+           
+        }
+
+         
+
+      
 
         return ans;
     }
