118689e4256f8444664ff9bcff6a5c1b6fe12142
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2025-08-14 21:53:01 +0530

    Time: 282 ms (5.40%) | Memory: 205.6 MB (6.60%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,25 +1,49 @@
 class Solution {
-    bool solve(vector<int>& nums, int target, int ind, int sum, vector<vector<int>>& dp) {
-        if(sum == target) return true;
-        if(ind >= nums.size() || sum > target) return false;
 
-        if(dp[ind][sum] != -1) return dp[ind][sum];
+    bool solve(vector<int>& nums, int target, int ind, int sum,vector<vector<int>>& dp) {
 
-        bool take = solve(nums, target, ind + 1, sum + nums[ind], dp);
-        bool notake = solve(nums, target, ind + 1, sum, dp);
+        if (sum == target) {
+            return true;
+        }
 
-        return dp[ind][sum] = take || notake;
+        if (ind >= nums.size()) {
+            return false;
+        }
+
+        if(dp[ind][sum]!=-1)
+        {
+            return dp[ind][sum];
+        }
+
+        sum+=nums[ind];
+
+        int take = solve(nums, target, ind + 1, sum,dp);
+
+        sum-=nums[ind];
+
+        int notake = solve(nums, target, ind + 1, sum,dp);
+
+        return dp[ind][sum] =  take | notake;
     }
 
 public:
     bool canPartition(vector<int>& nums) {
-        int total = accumulate(nums.begin(), nums.end(), 0);
-        if(total % 2 != 0) return false;
 
-        int target = total / 2;
+        int sum = accumulate(nums.begin(), nums.end(),0);
 
-        vector<vector<int>> dp(nums.size(), vector<int>(target + 1, -1));
+        if (sum % 2) {
+            return false;
+        }
 
-        return solve(nums, target, 0, 0, dp);
+        int target = sum / 2;
+
+        sum = 0;
+
+        int n= nums.size();
+
+        vector<vector<int>>dp(n+1,vector<int>(target*2,-1));
+
+        return solve(nums, target, 0, sum,dp);
+
     }
 };
