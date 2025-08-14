034c76f557df9251e833ad5159fc35ff9da88017
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2025-08-14 12:14:48 +0530

    Time: 251 ms (13.98%) | Memory: 29.2 MB (11.48%) - LeetSync

--- 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.cpp
@@ -1,53 +1,41 @@
 class Solution {
 public:
     int beautySum(string s) {
-        
-         int ans=0;
 
-        for(int i=0;i<s.size();i++)
-        {     
+         int ans = 0;
 
-            string s1="";
+        //  "xzvfsppsjfbxdwkqe"
 
-            unordered_map<char,int>mp;
+        for (int i = 0; i < s.size(); i++) {
 
+            map<char, int> mp;
+
+            int mini = INT_MAX;
+
+            int maxi = INT_MIN;
+
+            for (int j = i; j < s.size(); j++) {
+
+                mp[s[j]]++; 
+
+                int maxi = INT_MIN;
+
+                int mini = INT_MAX;
+
+                for(auto it:mp)
+                {
+                   maxi = max(maxi,it.second);
+                   mini = min(mini,it.second);
+                }
+
+                ans+=maxi-mini;
+ 
             
-
-            for(int j=i;j<s.size();j++)
-            {
-              
-               mp[s[j]]++; 
-
-              int maxi=INT_MIN,mini=INT_MAX,flag=0;
-
-              if(mp.size()>1)
-              {  
-                flag=1;
-               for(auto it:mp)
-               {
-                  maxi = max(maxi,it.second);
-
-                  mini = min(mini, it.second);
-               }
-              }
-                
-               
-              
-              
-               if(flag)
-               ans+=maxi-mini;
-          
-             
-             
-
-            //   cout <<s1<<" "<<maxi-mini<<endl;
-
             }
-
-        
         }
 
-         return ans;
+
+        return ans;
 
     }
 };
