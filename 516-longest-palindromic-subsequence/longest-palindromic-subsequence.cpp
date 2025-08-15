290a3e07bc66563ec6858c094278a55eadf95367
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2025-08-15 10:27:52 +0530

    Time: 87 ms (45.49%) | Memory: 76 MB (43.52%) - LeetSync

--- 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.cpp
@@ -1,41 +1,49 @@
-int dp[1001][1001];
-
 class Solution {
-private:
-    int solve(string& text1,string& text2,int i,int j)
-    {  
+        
+        int solve(string& s1, string& s2, int i ,int j,vector<vector<int>>& dp)
+        {
 
-        if(i==text1.length() || j==text2.length()) return 0;
+            if(s1.size()<=i  ||  j>=s2.size())
+            {
+                return 0;
+            }
 
-        int ans=0,ans1=0,ans2=0;
+            if(dp[i][j]!=-1)
+            {
+                return dp[i][j];
+            }
 
-         if(dp[i][j]!=-1) return dp[i][j];
+            int take = INT_MIN , notake = INT_MIN;
 
-        if(text1[i]==text2[j]) 
-        { 
+            if(s1[i]==s2[j])
+            {
+                take = 1+solve(s1,s2,i+1,j+1,dp);
+            }
 
-            ans = 1+solve(text1,text2,i+1,j+1);
+            else {
 
+                notake = max(solve(s1,s2,i+1,j,dp),solve(s1,s2,i,j+1,dp));
 
-        }else
-        { 
+            }
 
-            ans1 = solve(text1,text2,i+1,j),
-
-            ans2= solve(text1,text2,i,j+1);
+             return dp[i][j] =  max(take,notake);
 
         }
 
-        return  dp[i][j] = max({ans,ans1,ans2});
-
-    }
 
 public:
     int longestPalindromeSubseq(string s) {
-         memset(dp,-1,sizeof(dp));
-         string revstr=s;
-         reverse(revstr.begin(),revstr.end());
-         return solve(s,revstr,0,0);
 
+        string s1 = s;
+
+        string s2 = s1;
+
+        reverse(s2.begin(),s2.end());
+
+        vector<vector<int>>dp(s1.size(),vector<int>(s2.size(),-1));
+
+
+        return solve(s1,s2,0,0,dp);
+        
     }
 };
