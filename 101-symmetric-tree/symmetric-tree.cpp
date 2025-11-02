4ff3c102b15e816c2da6f8c6afa89d101e8a7fb2
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2025-11-02 19:18:28 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (29.47%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -10,38 +10,37 @@
  * };
  */
 class Solution {
-public:  
-  
-  bool match(TreeNode* root1, TreeNode* root2)
-  {
-    if(root1!=NULL && root2!=NULL)
-    {
-        bool a=match(root1->left,root2->right );
-         bool b=match(root1->right,root2->left );
-         if(a && b && root1->val ==root2->val)
-         {
-            return true;
-         }
-         else {
-            return false;
-         } 
-    }
-         if(root1==NULL && root2==NULL)
-         {
-            return true;
-         }
-         else {
-            return false;
-         }
-    
-  }
-    bool isSymmetric(TreeNode* root) {
-      if(root==NULL)
-      {
-        return true;
-      }
-      return match(root->left,root->right);
 
+   bool solve(TreeNode* node1 , TreeNode* node2)
+   {
+    if(node1 == NULL  &&  node2 == NULL)
+    {
+        return true;
+    }
+
+    if(node1!=NULL  && node2 !=NULL)
+    {
+       bool a = solve(node1->left , node2->right);
+       bool b = solve(node1->right , node2->left);
+
+       if(a && b && node1->val == node2->val)
+       {
+        return true;
+       }
+       else {
+        return false;
+       }
+    }
+    else {
+        return false;
+    }
+
+   }
+
+public:
+    bool isSymmetric(TreeNode* root) {
+        
+        return solve(root->left , root->right);
 
     }
 };
