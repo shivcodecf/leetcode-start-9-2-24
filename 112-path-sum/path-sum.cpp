23cd90bf060e059fa1c491ab93ca25af7f0a6bbc
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2026-01-15 00:25:22 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (33.37%) - LeetSync

--- 112-path-sum/path-sum.cpp
+++ 112-path-sum/path-sum.cpp
@@ -1,23 +1,43 @@
-#include <bits/stdc++.h>
-#define ll long long 
-#define nl '\n'
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
+    
+    bool solve(TreeNode* node , int target)
+    { 
+
+      if(node==nullptr)
+      {
+        return 0;
+      }
+
+       target-=node->val;
+
+      if(node->left==nullptr && node->right==nullptr)
+      {
+        return target == 0;
+      }
+
+      return solve(node->left,target) || solve(node->right,target);
+
+
+
+
+    }
+
+
 public:
     bool hasPathSum(TreeNode* root, int targetSum) {
-        if (root ==NULL) return false;
-        // Update the targetSum by subtracting the current node's value
-        targetSum-=root->val;
-           if (root->left == NULL && root->right == NULL) {
-            // If it's a leaf, check if the updated targetSum is zero
-            if(targetSum == 0){
-                return true;
-            }else{
-                return false;
-            }
-        }
 
-        // Recursively check the left and right subtree
-        return ( hasPathSum(root->left, targetSum)) ||
-               ( hasPathSum(root->right, targetSum));
+        return solve(root,targetSum);
+        
     }
 };
