bc5ff529e0bff6a05c5747a022492f5f49f05632
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2026-01-23 02:59:16 +0530

    Time: 8 ms (89.87%) | Memory: 17.3 MB (87.29%) - LeetSync

--- 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/lowest-common-ancestor-of-a-binary-tree.cpp
@@ -1,23 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
-    TreeNode* solve(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if (root == nullptr) return nullptr;
+    
+    TreeNode* solve(TreeNode* root , TreeNode* p , TreeNode* q)
+    {
+        if(root == nullptr)
+        {
+            return nullptr;
+        }
 
-        if (root->val == p->val || root->val == q->val) {
+        if(root == p || root == q)
+        {
             return root;
         }
 
-        TreeNode* left  = solve(root->left, p, q);
-        TreeNode* right = solve(root->right, p, q);
+        TreeNode*left = solve(root->left,p,q);
+        TreeNode*right = solve(root->right,p,q);
 
-        if (left && right) {
+        if(left && right)
+        {
             return root;
         }
-
-        return left ? left : right;
+        //   else if((root == p && left) || (root == p && right) || (root == q && right) || (root == q && left))
+        // {
+        //     return root;
+        // }
+        else if(left)
+        {
+            return left;
+        }
+        else if(right)
+        {
+            return right;
+        }
+          
+        return nullptr;
+        
     }
 
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        return solve(root, p, q);
+
+        return solve(root,p,q);
+        
     }
 };
