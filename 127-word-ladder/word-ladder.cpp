c066483453e9d70209be8afd2d6b951c08ab768b
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2026-01-31 16:59:07 +0530

    Time: 347 ms (23.75%) | Memory: 23.5 MB (21.26%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,74 +1,74 @@
 class Solution {
 public:
-    int ladderLength(string beginWord, string endWord,
-                     vector<string>& wordList) {
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
 
-        map<string, int> mp;
 
-        for (int i = 0; i < wordList.size(); i++) {
-            
+        
+        queue<pair<int, string>>q;
+
+        q.push({0, beginWord});
+
+        map<string,int>mp;
+
+        for(int i=0;i<wordList.size();i++)
+        {
             mp[wordList[i]]++;
         }
+        
+        vector<string>temp;
+        vector<vector<string>>ans;
 
-        queue<pair<string, int>> q;
+        int level = 0;
 
-        if(mp.find(endWord)==mp.end())
-        {
-            return 0;
-        }
-
-        q.push({beginWord,0});
-
-        int ans = 0;
+        temp.push_back(beginWord);
 
         while (!q.empty()) {
-
             int n = q.size();
+
+            level++;
 
             while (n--) {
 
-                string s = q.front().first;
-                int l = q.front().second;
+                auto [step, s] = q.front();
+
                 q.pop();
+
+                if(s == endWord)
+                {
+                  return step+1;
+                }
 
                 string s1 = s;
 
-                if(s==endWord)
-                {
-                    return l+1;
-                }
-
-               
-
-                int flag = 0;
-
-                for (int i = 0; i < s1.size(); i++) {
-
-                    for (char j = 'a'; j <= 'z'; j++) {
-
-                        s[i] = j;
+                for (int i = 0; i < s.size(); i++) {
+                    for (char ch = 'a'; ch <= 'z'; ch++) {
+                        s[i] = ch;
 
                         if(mp.find(s)!=mp.end())
-                        {  
-                            flag = 1;
-                            q.push({s,l+1});
+                        {
+                          q.push({level, s});
+                          temp.push_back(s);
+                          mp[s]--;
+
+                          if(mp[s]==0)
+                          {
                             mp.erase(s);
-                        }
-
-                        s = s1;
-
-
+                          }
+                        }     
+                        
+                      
+                       
                     }
+
+                    s = s1;
+
+                    
                 }
-
-               
-
             }
-
-
         }
 
         return 0;
+
 
         
     }
