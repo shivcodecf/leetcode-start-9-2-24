2882c82ce43d140bf8e2092325d50bc34f3ee22c
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2026-02-07 23:57:58 +0530

    Time: 243 ms (60.71%) | Memory: 10.7 MB (55.45%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,87 +1,76 @@
-/* In this problem, we need to search if a given word exists inside a board[][] of characters
-From one cell, we can move into it's 4 adjacent cells in the following directions - Down , Right , Up , Left
-We cannot use one cell for more than once. Thus, backtracking comes into the picture
-*/
-
 class Solution {
-private:
-    
-bool search(int i , int j , int n , int m , vector<vector<char>> &board , string &word , int k)
-{
-    // If k is equal to the given word's length, it means every character of word is present inside board[][], so we return true
-    
-    if(k == word.size() )
-       return true ;
-    
-    // If we are moving outside the board's boundary or if board[i][j] is not equal to word[k] , we return false
-    
-    if(i < 0 || j < 0 || i == n || j == m  || board[i][j] != word[k])
-        return false ;
-    
-    // We store the Character present in the current Cell inside ch variable 
-    
-    char ch = board[i][j] ;
-    
-    // We change board[i][j] to # so that we don't visit the Same Cell again
-    
-    board[i][j] = '#' ;
-    
-    // We move in Downward direction
-    
-    bool op1 = search(i + 1 , j , n , m , board , word , k + 1) ;
-    
-    // We move in Rightward direction
-    
-    bool op2 = search(i , j + 1 , n , m , board , word , k + 1) ;
-    
-    // We move in Upward direction
-    
-    bool op3 = search(i - 1 , j , n , m , board , word , k + 1) ;
-    
-    // We move in Leftward direction
-    
-    bool op4 = search(i , j - 1 , n , m , board , word , k + 1) ;
-    
-    // We backtrack and change the value of board[i][j] to it's original character stored in ch variable
-    
-    board[i][j] = ch ;
-    
-    // As we need to find the word, no matter from which direction we get it, so we return (op1 OR op2 OR op3 OR op4)
-    
-    return op1 || op2 || op3 || op4 ;
-}
+
+    bool solve(vector<vector<char>>& board, string& word, string& s, int i,
+               int j, int pointer, vector<vector<int>>& vis) {
+        int n = board.size(), m = board[0].size();
+
+        int len = word.size();
+
+        vis[i][j] = 1;
+
+        s += board[i][j];
+
+        if (pointer == len - 1) {
+            bool ok = (s == word);
+            vis[i][j] = -1;
+            s.pop_back();
+            return ok;
+        }
+
+        bool l = false, r = false, t = false, b = false;
+
+        if (i - 1 < n && j < m && i - 1 >= 0 && j >= 0 && vis[i - 1][j] == -1 &&
+            board[i - 1][j] == word[pointer + 1]) {
+
+            l = solve(board, word, s, i - 1, j, pointer + 1, vis);
+        }
+
+        if (i + 1 < n && j < m && i + 1 >= 0 && j >= 0 && vis[i + 1][j] == -1 &&
+            board[i + 1][j] == word[pointer + 1]) {
+            r = solve(board, word, s, i + 1, j, pointer + 1, vis);
+        }
+
+        if (i < n && j + 1 < m && i >= 0 && j + 1 >= 0 && vis[i][j + 1] == -1 &&
+            board[i][j + 1] == word[pointer + 1]) {
+
+            t = solve(board, word, s, i, j + 1, pointer + 1, vis);
+        }
+
+        if (i < n && j - 1 < m && i >= 0 && j - 1 >= 0 && vis[i][j - 1] == -1 &&
+            board[i][j - 1] == word[pointer + 1]) {
+
+            b = solve(board, word, s, i, j - 1, pointer + 1, vis);
+        }
+
+        vis[i][j] = -1;
+
+        if (s.size()) {
+            s.pop_back();
+        }
+
+        return l | r | t | b;
+    }
 
 public:
-    bool exist(vector<vector<char>>& board, string &word) {
-        
-    int n = board.size() ;
-    int m = board[0].size() ;
-        
-        
-    for(int i = 0 ; i < n ; i++)
-    {
-        for(int j = 0 ; j < m ; j++)
-        {
-            // We start checking only if board[i][j] matches with word[0]
-            
-            if(board[i][j] == word[0] )
-            {
-                // If our search results return True, we directly return true 
-                
-                if(search(i , j , n , m , board , word , 0) )
-                    return true ;
+    bool exist(vector<vector<char>>& board, string word) {
+
+        int n = board.size(), m = board[0].size();
+
+        vector<vector<int>> vis(n, vector<int>(m, -1));
+
+        string s = "";
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (word[0] == board[i][j]) {
+
+                    if (solve(board, word, s, i, j, 0, vis)) {
+                        return true;
+                    }
+                }
             }
         }
-    }
-    
-    // Otherwise, if we searched the entire matrix and still didn't find the word, we return false 
-    
-    return false ; 
-}
-};
 
-/*
-Time Complexity:  O(N * M * 4^K)
-Space Complexity: O(K)
-K = word.size()
-*/
+        return false;
+    }
+};
