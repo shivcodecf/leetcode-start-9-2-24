f572716d5ab4667f67b303b2cf55ce02687ad8df
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2026-02-21 16:54:53 +0530

    Time: 186 ms (15.23%) | Memory: 110.3 MB (44.83%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,49 +1,56 @@
 class Solution {
-
-    bool solve(vector<int>& nums, int target, int ind, int sum,vector<vector<int>>& dp) {
-
-        if (sum == target) {
-            return true;
-        }
-
-        if (ind >= nums.size()) {
-            return false;
-        }
-
-        if(dp[ind][sum]!=-1)
-        {
-            return dp[ind][sum];
-        }
-
-        sum+=nums[ind];
-
-        int take = solve(nums, target, ind + 1, sum,dp);
-
-        sum-=nums[ind];
-
-        int notake = solve(nums, target, ind + 1, sum,dp);
-
-        return dp[ind][sum] =  take | notake;
+  
+  bool solve(vector<int>& nums,int target,int sum,int ind,vector<vector<int>>& dp)
+  { 
+   
+    if(sum == target)
+    {
+        return true;
     }
+
+    if(sum>target)
+    {
+        return false;
+    }
+
+    if(ind>=nums.size())
+    {
+        return false;
+    }
+
+    if(dp[ind][sum]!=-1)
+    {
+        return dp[ind][sum];
+    }
+
+    bool take = false , notake = false;
+
+    take = solve(nums,target,sum+nums[ind],ind+1,dp);
+
+    notake = solve(nums,target,sum,ind+1,dp);
+
+    return dp[ind][sum] =  take || notake;
+
+
+  }
 
 public:
     bool canPartition(vector<int>& nums) {
 
-        int sum = accumulate(nums.begin(), nums.end(),0);
+        int sum = accumulate(nums.begin(),nums.end(),0);
 
-        if (sum % 2) {
+        if(sum & 1)
+        {
             return false;
         }
 
-        int target = sum / 2;
+        int target = sum/2;
 
-        sum = 0;
+        int n = nums.size();
 
-        int n= nums.size();
+        vector<vector<int>>dp(n,vector<int>(target+1,-1));
 
-        vector<vector<int>>dp(n+1,vector<int>(target*2,-1));
-
-        return solve(nums, target, 0, sum,dp);
-
+        return solve(nums,target,0,0,dp);
+        
     }
 };
