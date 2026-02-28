57d5217c92fbc367e828c8ce52638b0ac72a67aa
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2026-02-28 15:30:02 +0530

    Time: 5 ms (7.59%) | Memory: 11.3 MB (19.16%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,19 +1,49 @@
 class Solution {
+   
+   vector<vector<int>>ans;
+   map<int,int>mp;
+
+   void solve(vector<int>& nums,vector<int>& v)
+   {  
+
+     int n = nums.size();
+
+     if(v.size()==n)
+     {
+        ans.push_back(v);
+        return;
+     }
+
+     for(int i=0;i<n;i++)
+     {  
+
+        if(mp.find(nums[i])==mp.end())
+        {
+             v.push_back(nums[i]);
+             mp[nums[i]]++;
+             solve(nums,v);
+             v.pop_back();
+             mp.erase(nums[i]);
+        }
+
+        
+
+     }
+
+
+
+   }
+
 public:
     vector<vector<int>> permute(vector<int>& nums) {
 
-       vector<vector<int>>res;
+        
 
-       sort(nums.begin(),nums.end());
+        vector<int>v;
 
-       do{
-         res.push_back(nums);
-       }while(
-        next_permutation(nums.begin(),nums.end())
-       );
+        solve(nums,v);
 
-       return res;
-
-
+        return ans;
+        
     }
 };
