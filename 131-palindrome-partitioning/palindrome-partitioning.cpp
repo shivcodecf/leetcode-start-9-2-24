4292e2d7cbec5c4eeb9c20d0b9966b9c9fd35cb4
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2026-03-03 21:26:25 +0530

    Time: 55 ms (46.51%) | Memory: 80 MB (25.10%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/palindrome-partitioning.cpp
@@ -1,44 +1,53 @@
 class Solution {
+    vector<vector<string>> ans;
 
-    
+    bool isPalindrome(string& temp) {
 
-public:  
-      vector<vector<string>> ans;  // Global variable to store the final result
-      string s1="";
+        int i = 0, j = temp.size() - 1;
 
-bool pal( string& s2) { 
-    int i = 0, j = s2.size() - 1;
-    while (i < j) {
-        if (s2[i] != s2[j]) {
-            return false;
+        while (i < j) {
+
+            if (temp[i] != temp[j]) {
+                return false;
+            }
+
+            i++;
+
+            j--;
         }
-        i++;
-        j--;
-    }
-    return true;
-}
 
-void solve(string& s, int ind, vector<string>& v1) { 
-    if (s.size() == ind) {
-        ans.push_back(v1);
-        return;
+        return true;
     }
 
-    for (int i = ind; i < s.size(); i++) {  
-         string s1 = s.substr(ind, i - ind + 1);  // Extract the substring
-        if (pal(s1)) {   
-            v1.push_back(s1);  // Add the palindrome substring to the current partition
-            solve(s, i + 1, v1);  // Recur for the next part of the string
-            v1.pop_back();  // Backtrack
+    void solve(string& s, int ind, vector<string>& v) {
+        if (ind >= s.size()) {
+            if (v.size()) {
+                ans.push_back(v);
+            }
+        }
+
+        for (int i = ind; i < s.size(); i++) {
+
+            string temp = s.substr(ind, i-ind+1);
+
+            if (isPalindrome(temp)) {
+
+                v.push_back(temp);
+
+                solve(s, i+1, v);
+
+                v.pop_back();
+            }
         }
     }
-}
 
-vector<vector<string>> partition(string s) {
-    ans.clear();  // Clear the global result before starting
-    vector<string> v1;
-    
-    solve(s, 0, v1);
-    return ans;
-}
+public:
+    vector<vector<string>> partition(string s) {
+
+        vector<string> v;
+
+        solve(s, 0, v);
+
+        return ans;
+    }
 };
