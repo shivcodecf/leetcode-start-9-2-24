549b048f05ea82fde8416a198df860db0391cdeb
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2026-03-06 17:09:06 +0530

    Time: 22 ms (12.32%) | Memory: 18 MB (7.13%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -1,51 +1,51 @@
 class Solution {
-    private:
+    
+    int solve(string& word1,string& word2,int i,int j, vector<vector<int>>& dp)
+    {
 
-    int solve(string& s1,string& s2,int i,int j, vector<vector<int>>& dp)
-    {  
-        int n = s1.size(),m=s2.size();
-        if(i==n || j==m)
+     int n = word1.size(),m=word2.size();   
+  
+     if(i>=n || j>=m)
+     {
+        if(i<=n-1)
         {
-            if(i==n)
-            {
-                return abs(((m-1)-j)+1);
-            }
-            else {
-                return abs(((n-1)-i)+1);
-            }
+            return n-1-i+1;
         }
-
-        if(dp[i][j]!=INT_MAX) return dp[i][j];
-
-        int take1=INT_MAX,take2=INT_MAX,take3=INT_MAX;
-
-        if(s1[i]==s2[j])
+         if(j<=m-1)
         {
-           take1 = solve(s1,s2,i+1,j+1,dp);
+            return m-1-j+1;
         }
 
-        else {
+        return 0;
+     }
 
-             take2 =  1+solve(s1,s2,i+1,j,dp);
+     if(dp[i][j]!=-1)
+     {
+        return dp[i][j];
+     }
+     
+     int equal = INT_MAX , del1=INT_MAX, del2= INT_MAX;
+     if(word1[i]==word2[j])
+     {
+      equal = solve(word1,word2,i+1,j+1,dp);
+     }
+     else {
+       del1 = 1+solve(word1,word2,i,j+1,dp);
+       del2 = 1+solve(word1,word2,i+1,j,dp);
+     }
 
-             take3 =  1+solve(s1,s2,i,j+1,dp);
-
-        }
-
-        return dp[i][j]=min({take1,take2,take3});
-
-
+     return dp[i][j] = min({equal,del1,del2});
 
     }
 
 public:
     int minDistance(string word1, string word2) {
-          int n = word1.size(),m=word2.size();
 
-        vector<vector<int>>dp(n+1,vector<int>(m+1,INT_MAX));
+         int n = word1.size(),m=word2.size();
+
+        vector<vector<int>>dp(n,vector<int>(m,-1));
 
         return solve(word1,word2,0,0,dp);
-
-
+        
     }
 };
