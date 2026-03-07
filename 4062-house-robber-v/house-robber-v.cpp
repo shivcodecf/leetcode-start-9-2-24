eb891f1db2d57c6f0e9f89e53a0bb9be2cfacae1
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2026-03-07 17:38:03 +0530

    Time: 45 ms (24.57%) | Memory: 243.7 MB (72.42%) - LeetSync

--- 4062-house-robber-v/house-robber-v.cpp
+++ 4062-house-robber-v/house-robber-v.cpp
@@ -1,33 +1,36 @@
 class Solution {
-    
-    long long solve(int ind, int prevTaken, vector<int>& nums, vector<int>& colors, vector<vector<long long>>& dp) {
+public:
+    int n;
+    long long dp[100001];
+    long long solve(int ind, vector<int>&nums,vector<int>&colors)
+    {
+        if(ind>=n) return 0;
+        if(dp[ind]!=-1) return dp[ind];
         
-        int n = nums.size();
+        //if it is the last house -> best to choose robbing this house
+        if(ind==n-1) {
+            return nums[ind] + solve(ind+1,nums,colors);
+        }
         
-        if(ind >= n)
-            return 0;
+        long long val1=0; long long val2=0;
 
-        if(dp[ind][prevTaken] != -1)
-            return dp[ind][prevTaken];
-
-        long long notTake = solve(ind + 1, 0, nums, colors, dp);
-
-        long long take = 0;
-
-        if(ind == 0 || !(prevTaken && colors[ind] == colors[ind-1])) {
-            take = nums[ind] + solve(ind + 1, 1, nums, colors, dp);
+        //if chosen to rob -> must skip next house if same color or 
+        
+        if(colors[ind]==colors[ind+1]){
+            val1 = nums[ind]+solve(ind+2,nums,colors);
+        }else{
+            val1 = nums[ind]+solve(ind+1,nums,colors);
         }
 
-        return dp[ind][prevTaken] = max(take, notTake);
+        //if chosen not to rob
+        val2 = solve(ind+1,nums,colors);
+
+        return dp[ind]=max(val1,val2);
+        
     }
-
-public:
     long long rob(vector<int>& nums, vector<int>& colors) {
-
-        int n = nums.size();
-
-        vector<vector<long long>> dp(n, vector<long long>(2, -1));
-
-        return solve(0, 0, nums, colors, dp);
+        memset(dp,-1,sizeof(dp));
+        n = nums.size();
+        return solve(0,nums,colors);
     }
 };
