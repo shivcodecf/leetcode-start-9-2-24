85ce564dadafbf1912e6853ea8f5b438f929105b
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2026-03-07 17:51:03 +0530

    Time: 34 ms (28.24%) | Memory: 254 MB (30.67%) - LeetSync

--- 4062-house-robber-v/house-robber-v.cpp
+++ 4062-house-robber-v/house-robber-v.cpp
@@ -1,49 +1,37 @@
 class Solution {
 
-     long long dp[100001];
-
-    long long solve(vector<int>& nums, vector<int>& colors, int ind) {
+    long long solve(vector<int>& nums, vector<int>& colors, int ind,
+              vector<long long>& dp) {
         int n = nums.size();
 
-        // if(ind == n-1)
-        // {
-        //     return nums[ind];
-        // }
-
-        if (ind >= n) {
-            return 0;
-        }
-
-           if (dp[ind] != -1) {
-            return dp[ind];
-        }
-
-        if(ind==n-1)
+        if(ind == n-1)
         {
             return nums[ind];
         }
 
+        if (ind >=n) {
+            return 0;
+        }
+
         long long take = 0, notake = 0, adjTake = 0;
 
-     
-
-        notake = solve(nums, colors, ind + 1);
-
-        int nextIdx = -1;
-
-        if (colors[ind] == colors[ind + 1]) {
-
-            nextIdx = ind+2;
-
-        }
-        else {
-            nextIdx = ind+1;
+        if (dp[ind] != -1) {
+            return dp[ind];
         }
 
-        take = nums[ind] +  solve(nums, colors,  nextIdx);
+        notake = solve(nums, colors, ind + 1, dp);
 
+        if (ind < n - 1 && colors[ind] == colors[ind + 1]) {
 
-        return dp[ind] = max(take, notake);
+            take = nums[ind] + solve(nums, colors, ind + 2, dp);
+
+        } else {
+
+            adjTake = nums[ind] + solve(nums, colors, ind + 1, dp);
+
+        }
+
+        return dp[ind] = max(take, max(adjTake, notake));
     }
 
 public:
@@ -51,8 +39,8 @@ public:
 
         int n = nums.size();
 
-        memset(dp,-1,sizeof(dp));
+        vector<long long> dp(n, -1);
 
-        return solve(nums, colors, 0);
+        return solve(nums, colors, 0, dp);
     }
 };
