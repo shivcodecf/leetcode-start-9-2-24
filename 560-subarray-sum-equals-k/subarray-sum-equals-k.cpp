11ef419dcd7f96d8da87fe63a01210d8d7b4e454
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2026-03-10 18:58:19 +0530

    Time: 66 ms (32.45%) | Memory: 49.3 MB (21.54%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -11,50 +11,45 @@ public:
         vector<int> pref(n, 0);
 
         pref[0] = nums[0];
-        
+
+        // why we need this (mp[0]=1), below is tc 
+        // [1,-1]
+        // below is prefix Sum
+        // [1,0] --> here if we not store (mp[0]=1) then , we not get my answer. that its, we need .
+
         mp[0] = 1;
 
-        // [1,-1,0] --->  [1,0,0]   k = 0;
 
-       
+
+
+        
+
         for (int i = 1; i < nums.size(); i++) {
             pref[i] = pref[i - 1] + nums[i];
-
-           
         }
 
         for (int i = 0; i < pref.size(); i++) {
 
-            // if (pref[i] == k) {
+          
 
-            //     ans++;
+            int left = pref[i] - k;
 
-            // }
-           
+            if (mp.find(left) != mp.end()) {
 
-                int left = pref[i] - k;
+           // why here , we need to add all required frequencies, here is the tc which will clear all doubts
 
-                if (mp.find(left) != mp.end()) {
+         //  --> [1,-1,0]  --> prefSum = [1,0,0] 
+         // here , if we not take all prev frequencies , then its will give wrong answer.here is few also expamles
 
-                    ans+=mp[left];
+         // [2,3,5,-10,0] --> [2,5,10,0,0] --> 
 
-                   
+                ans += mp[left];
 
-                    // if (mp[left] == 0) {
-                    //     mp.erase(left);
-                    // }
+                
+            }
 
-                }
-            
-
-              mp[pref[i]]++;
-
-           
+            mp[pref[i]]++;
         }
-
-         
-
-      
 
         return ans;
     }
