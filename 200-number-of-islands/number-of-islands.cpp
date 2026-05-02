5b34cb4b06c26a75eb9435edf390762e4a3942d7
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2026-05-02 16:24:38 +0530

    Time: 27 ms (55.70%) | Memory: 17.3 MB (48.88%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,55 +1,44 @@
 class Solution {
-      
-      void dfs(vector<vector<char>>& grid ,vector<vector<int>>& vis,int x, int y )
-      { 
 
-          int n = grid.size(),m=grid[0].size();
+    void solve(vector<vector<char>>& grid, vector<vector<int>>& vis, int x, int y) {
 
-        vis[x][y] = 1;
+    int n = grid.size(), m = grid[0].size();
 
-        int dx[] = {0,1,0,-1};
+    vis[y][x] = 1;
 
-        int dy[] = {1,0,-1,0};
+    int dx[] = {0, +1, 0, -1};
+    int dy[] = {+1, 0, -1, 0};
 
-        for(int i=0;i<4;i++)
-        {
-            int newR = x+dx[i];
+    for (int i = 0; i < 4; i++) {
+        int newX = x + dx[i];
+        int newY = y + dy[i];
 
-            int newC = y+dy[i];
+        if (newX >= 0 && newY >= 0 && newX < m && newY < n &&
+            grid[newY][newX] == '1' && vis[newY][newX] == -1) {
 
-            if( newR >=0 && newC>=0 && newR <n && newC<m && vis[newR][newC]==-1 && grid[newR][newC]=='1' )
-            {
-                dfs(grid,vis,newR,newC);
-            }
+            solve(grid, vis, newX, newY);
         }
-
-
-
-
-      }
+    }
+}
 
 public:
     int numIslands(vector<vector<char>>& grid) {
 
-        int n = grid.size(),m=grid[0].size();
+        int n = grid.size(), m = grid[0].size();
 
-        vector<vector<int>>vis(n,vector<int>(m,-1));
+        vector<vector<int>> vis(n, vector<int>(m, -1));
 
         int ans = 0;
 
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<m;j++)
-            {
-               if(vis[i][j]== -1 && grid[i][j]=='1')
-               {
-                  dfs(grid,vis,i,j);
-                  ans++;
-               }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (vis[i][j] == -1 && grid[i][j]=='1') {
+                    solve(grid, vis, j, i);
+                    ans++;
+                }
             }
         }
 
         return ans;
-        
     }
 };
