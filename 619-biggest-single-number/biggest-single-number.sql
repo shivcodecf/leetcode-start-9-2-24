daec85c8309a2c23b45b3fde420c9c4404e259e6
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2026-05-02 22:43:56 +0530

    Time: 467 ms (18.51%) | Memory: 0B (100.00%) - LeetSync

--- 619-biggest-single-number/biggest-single-number.sql
+++ 619-biggest-single-number/biggest-single-number.sql
@@ -1,9 +1,8 @@
-SELECT IFNULL(
-    (SELECT num 
-     FROM MyNumbers 
-     GROUP BY num 
-     HAVING COUNT(*) = 1 
-     ORDER BY num DESC 
-     LIMIT 1), 
-    NULL
-) AS num;
+SELECT IFNULL((
+    SELECT num
+    FROM MyNumbers
+    GROUP BY num
+    HAVING COUNT(num) = 1
+    ORDER BY num DESC
+    LIMIT 1
+), NULL) AS num;
