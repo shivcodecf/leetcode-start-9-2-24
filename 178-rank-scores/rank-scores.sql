f5fbb6c1345fc461bb96e1a4247954c92ac523b6
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2026-05-04 00:13:24 +0530

    Time: 277 ms (95.83%) | Memory: 0B (100.00%) - LeetSync

--- 178-rank-scores/rank-scores.sql
+++ 178-rank-scores/rank-scores.sql
@@ -1,2 +1,2 @@
--- Write your PostgreSQL query statement below
-select score , DENSE_RANK() over(order by score DESC) as "rank" from Scores;
+SELECT score, DENSE_RANK() OVER (ORDER BY score DESC) AS `rank`
+FROM Scores;
