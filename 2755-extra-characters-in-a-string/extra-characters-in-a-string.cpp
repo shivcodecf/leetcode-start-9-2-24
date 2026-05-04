46ab0a3696ca326c64cbad36d94e7adc0e4e4504
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2026-05-04 21:34:49 +0530

    Time: 54 ms (72.95%) | Memory: 70.1 MB (71.64%) - LeetSync

--- 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
+++ 2755-extra-characters-in-a-string/extra-characters-in-a-string.cpp
@@ -12,32 +12,34 @@ class Solution {
         int ans = INT_MAX;
         string curr = "";
 
+        // Otherwise count as extra character
+          ans = 1 + solve(s, ind + 1);
+
         for (int i = ind; i < s.size(); i++) {
             curr += s[i];
 
             // If substring exists in dictionary → no extra cost
             if (st.count(curr)) {
                 ans = min(ans, solve(s, i + 1));
-            } else {
-                // Otherwise count as extra character
-                ans = min(ans, (i - ind + 1) + solve(s, i + 1));
             }
+
+            // n*(n) = n*n  // in case of dp only
+
+           
         }
 
-        // n*(n) = n*n  // in case of dp only
-
-        return dp[ind] = ans;
+         return dp[ind] = ans;
     }
 
-public:
-    int minExtraChar(string s, vector<string>& dictionary) {
-        for (auto& word : dictionary) {
-            st.insert(word);
+    public:
+        int minExtraChar(string s, vector<string> & dictionary) {
+            for (auto& word : dictionary) {
+                st.insert(word);
+            }
+
+            int n = s.size();
+            dp.resize(n, -1);
+
+            return solve(s, 0);
         }
-
-        int n = s.size();
-        dp.resize(n, -1);
-
-        return solve(s, 0);
-    }
-};
+    };
