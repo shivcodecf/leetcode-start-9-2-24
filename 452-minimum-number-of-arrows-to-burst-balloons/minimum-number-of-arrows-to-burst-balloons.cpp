aaeba54f7e7195d1723e5b3eab40ba69486c81e7
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2026-05-11 11:53:43 +0530

    Time: 50 ms (44.61%) | Memory: 93.8 MB (73.17%) - LeetSync

--- 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cpp
+++ 452-minimum-number-of-arrows-to-burst-balloons/minimum-number-of-arrows-to-burst-balloons.cpp
@@ -2,22 +2,33 @@ class Solution {
 public:
     int findMinArrowShots(vector<vector<int>>& points) {
 
-        // Sort the balloons based on their end coordinates
-        sort(points.begin(), points.end(), [](const vector<int>& a, const vector<int>& b) {
-            return a[1] < b[1];
-        });
+        // [1,2,3,4,5,6,7,8]
 
-        int arrows = 1;
-        int prevEnd = points[0][1];
+        // []
 
-        // Count the number of non-overlapping intervals
-        for (int i = 1; i < points.size(); ++i) {
-            if (points[i][0] > prevEnd) {
-                arrows++;
-                prevEnd = points[i][1];
+        sort(points.begin(), points.end());
+
+        int n = points.size();
+
+        int prevMin = points[0][0];
+
+        int prevMax = points[0][1];
+
+        int ans = 1;
+
+        for (int i = 1; i < n; i++) {
+
+            if (prevMin <= points[i][0] && prevMax >= points[i][0]) {
+                prevMin = min(prevMin, points[i][0]);
+                prevMax = min(prevMax, points[i][1]);
+
+            } else {
+                ans++;
+                prevMin = points[i][0];
+                prevMax = points[i][1];
             }
         }
 
-        return arrows;
+        return ans;
     }
 };
