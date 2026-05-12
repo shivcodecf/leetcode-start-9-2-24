1a81b64e9d88865423b50d554a1bf6206ad10500
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2026-05-12 22:37:17 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (74.37%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -2,38 +2,40 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
 
-        vector<vector<int>>ans;
+       // use dynmic newIntervals
 
-        int i=0;
+       int n = intervals.size();
 
-        int n = intervals.size();
+       vector<vector<int>>ans;
 
-        while(i<n)
+       int i=0;
+
+       while(i<n)
+       {
+        if(intervals[i][0]>newInterval[1])
         {
-          if(intervals[i][0]>newInterval[1])
-          {
             break;
-          }
-          else if(newInterval[0]>intervals[i][1])
-          {
-            ans.push_back(intervals[i]);
-          }
-          else {
-            newInterval[0] = min(newInterval[0],intervals[i][0]);
-            newInterval[1] = max(newInterval[1],intervals[i][1]);
-          }
-          i++;
         }
-
-        ans.push_back(newInterval);
-
-        while(i<n)
+        else if(intervals[i][1]<newInterval[0])
         {
-            ans.push_back(intervals[i]);
-            i++;
+         ans.push_back(intervals[i]);
         }
+        else {
+          newInterval[0] = min(newInterval[0],intervals[i][0]);
+          newInterval[1] = max(newInterval[1],intervals[i][1]);  
+        }
+        i++;
+       } 
 
-        return ans;
+       ans.push_back(newInterval);
+
+       while(i<n)
+       {
+          ans.push_back(intervals[i]);
+          i++;
+       }
+
+       return ans;
         
     }
 };
