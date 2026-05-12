08a4fcdd0e3a46dc23efacbe0d397daf1f4cfb2a
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2026-05-12 12:17:57 +0530

    Time: 2 ms (94.35%) | Memory: 13 MB (81.00%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,19 +1,20 @@
 class Solution {
+public:
+    
+    int solve(string& word1 , string& word2 , int i , int j,vector<vector<int>>& dp)
+    {  
+        int n = word1.size(),m=word2.size();
 
-    int solve(string& word1, string& word2, int i, int j, vector<vector<int>>& dp) {
-        int n = word1.size(), m = word2.size();
-
-        if (i >= n || j >= m) {
-
-            if (j <= m - 1) {
-                return (m - 1 - j + 1);
+        if(i>=n || j>=m)
+        {
+            if(j>=m)
+            {
+                return n-i;
             }
-             if (i <= n - 1) {
-                return (n - 1 - i + 1);
+            else {
+               return m-j; 
             }
-
-
-            return 0;
+           
         }
 
         if(dp[i][j]!=-1)
@@ -21,27 +22,31 @@ class Solution {
             return dp[i][j];
         }
 
-        int insert = INT_MAX, update = INT_MAX, del = INT_MAX, skip = INT_MAX;
+        int take1=INT_MAX,take2=INT_MAX;
 
-        if (word1[i] == word2[j]) {
-            skip = solve(word1, word2, i + 1, j + 1,dp);
-        } else {
-            insert = 1 + solve(word1, word2, i, j + 1,dp);
-            update = 1 + solve(word1, word2, i + 1, j + 1,dp);
-            del = 1 + solve(word1, word2, i + 1, j,dp);
+        if(word1[i]==word2[j])
+        {
+            take1 = solve(word1,word2,i+1,j+1,dp);  
+        }
+        else {
+           take2 = 1+min(solve(word1,word2,i,j+1,dp),min(solve(word1,word2,i+1,j,dp),solve(word1,word2,i+1,j+1,dp))); 
         }
 
-        return  dp[i][j] = min({skip, insert, update, del});
+        return dp[i][j] =  min(take1,take2);
+
+
 
     }
 
-public:
+
     int minDistance(string word1, string word2) {
+       
+       int n = word1.size(),m=word2.size();
 
-        int n = word1.size(),m = word2.size();
+       vector<vector<int>>dp(n,vector<int>(m,-1));
+        
+        
+        return solve(word1,word2,0,0,dp);
 
-        vector<vector<int>>dp(n,vector<int>(m,-1));
-
-        return solve(word1, word2, 0, 0,dp);
     }
 };
