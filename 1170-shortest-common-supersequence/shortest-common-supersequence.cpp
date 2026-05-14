ca077cfd36371c9dbd58b3ac8af20d3b64cc98df
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2026-05-14 11:30:13 +0530

    Time: 27 ms (39.43%) | Memory: 26.9 MB (17.74%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -1,72 +1,75 @@
 class Solution {
-public:
-    vector<vector<int>> dp;
 
-    // Returns length of shortest common supersequence of
-    // str1[i:] and str2[j:]
-    int solve(string& str1, string& str2, int i, int j) {
-        int n = str1.size();
-        int m = str2.size();
+    int solve(string& str1, string& str2, int i, int j,vector<vector<int>>& dp) {
+        int n = str1.size(), m = str2.size();
 
-        // Base cases
-        if (i == n) return m - j;
-        if (j == m) return n - i;
+        if (i >= n) {
+            return m - j;
+        }
+        if (j >= m) {
+            return n - i;
+        }
 
-        // Memoized result
-        if (dp[i][j] != -1) {
+        if(dp[i][j]!=-1)
+        {
             return dp[i][j];
         }
 
-        // Characters match
-        if (str1[i] == str2[j]) {
-            return dp[i][j] = 1 + solve(str1, str2, i + 1, j + 1);
-        }
+        int equal = 0, first = 0, second = 0;
 
-        // Characters differ
-        return dp[i][j] =
-            1 + min(
-                solve(str1, str2, i + 1, j),   // take str1[i]
-                solve(str1, str2, i, j + 1)    // take str2[j]
-            );
+        if (str1[i] == str2[j]) {
+           return  dp[i][j]  = 1 + solve(str1, str2, i + 1, j + 1,dp);
+        } 
+
+        first = 1 +  min(solve(str1, str2, i + 1, j,dp),solve(str1, str2, i, j + 1,dp));
+            // second = 1 + solve(str1, str2, i, j + 1,dp);
+        
+
+        return dp[i][j] =  first;
     }
 
+public:
     string shortestCommonSupersequence(string str1, string str2) {
-        int n = str1.size();
-        int m = str2.size();
 
-        // Memo table stores only lengths
-        dp.assign(n + 1, vector<int>(m + 1, -1));
+        int n = str1.size(), m = str2.size();
 
-        // Fill memo table
-        // solve(str1, str2, 0, 0);
+        vector<vector<int>> dp(n, vector<int>(m, -1));
 
-        // Reconstruct answer using dp table
+        // solve(str1, str2, 0, 0,dp);
+
         int i = 0, j = 0;
+
         string ans = "";
 
         while (i < n && j < m) {
-            // Same character: include once
             if (str1[i] == str2[j]) {
                 ans += str1[i];
                 i++;
                 j++;
-            }
-            // Take character from str1
-            else if (solve(str1, str2, i + 1, j) <=
-                     solve(str1, str2, i, j + 1)) {
-                ans += str1[i];
-                i++;
-            }
-            // Take character from str2
-            else {
-                ans += str2[j];
-                j++;
+            } else {
+                if (solve(str1, str2, i + 1, j,dp) <=
+                    solve(str1, str2, i, j + 1,dp)) {
+                    ans += str1[i];
+                    i++;
+                    // j++;
+                } else {
+                    ans += str2[j];
+                    j++;
+                    // i++;
+                }
             }
         }
 
-        // Append remaining characters
-        while (i < n) ans += str1[i++];
-        while (j < m) ans += str2[j++];
+        while(i<n)
+        {
+            ans+=str1[i];
+            i++;
+        }
+         while(j<m)
+        {
+            ans+=str2[j];
+            j++;
+        }
 
         return ans;
     }
