13ece45df8a2537c64fd51821e3b931990fe479b
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2026-05-15 12:15:11 +0530

    Time: 637 ms (5.02%) | Memory: 414 MB (6.82%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,38 +1,44 @@
-int dp[10001];
-
 class Solution {
 
-    int solve(vector<int>& nums,int ind)
-    {  
+    int solve(vector<int>& nums, int ind, int check, vector<vector<int>>& dp) {
+        int n = nums.size();
 
-        int n=nums.size();
-
-        if(ind>=n) return INT_MAX;
-
-        if(ind==n-1)  return 0;
-
-        long long ans = INT_MAX;
-
-        if(dp[ind]!=-1) return dp[ind];
-
-        for(int i=1;i<=nums[ind];i++)
-        {
-            ans = min((long long)ans,1+(long long)solve(nums,ind+i));
+        if (ind == n - 1) {
+            return 1;
         }
 
-        return dp[ind] = ans;
+        if (ind >= n) {
+            return INT_MAX;
+        }
 
+        if (dp[ind][check] != -1) {
+            return dp[ind][check];
+        }
 
+        int skip = INT_MAX, jump = INT_MAX, jump1 = INT_MAX;
+
+        if (check - 1 <= 0) {
+            jump = 1 + solve(nums, ind + 1, nums[ind], dp);
+        } else {
+            skip = solve(nums, ind + 1, check - 1, dp);
+
+            jump1 = 1 + solve(nums, ind + 1, nums[ind], dp);
+        }
+
+        return dp[ind][check] = min(skip, min(jump, jump1));
     }
 
 public:
     int jump(vector<int>& nums) {
-        
-         int n=nums.size();
 
-         memset(dp,-1,sizeof(dp));
+        int n = nums.size();
 
-         return solve(nums,0);
+        int mx = *max_element(nums.begin(), nums.end());
 
+        vector<vector<int>> dp(n, vector<int>(mx + 1, -1));
+
+        int ans = solve(nums, 1, nums[0], dp);
+
+        return ans == INT_MAX ? 0 : ans;
     }
 };
