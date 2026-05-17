6ebaa259b11ca6ba3bfd6c5c1bd2aba6858c1748
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2026-05-17 20:42:24 +0530

    Time: 3 ms (38.08%) | Memory: 21.1 MB (12.61%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,65 +1,49 @@
 class Solution {
-      
-      void dfs(int node,vector<int>& vis, vector<vector<int>>&adj)
-      {
-        vis[node] = 1;
 
-        for(auto it:adj[node])
-        {
-          if(!vis[it])
-          {
-            dfs(it,vis,adj);
-          }
+    void dfs(unordered_map<int, vector<int>>& adj, int u, vector<bool>& vis) {
+        vis[u] = true;
+
+        for (auto it : adj[u]) {
+
+            if(!vis[it])
+            {
+                vis[it] = true;
+                dfs(adj,it,vis);
+
+            }
+            
         }
-
-      }
-
+    }
 
 public:
     int findCircleNum(vector<vector<int>>& isConnected) {
-        
-        int n=isConnected.size();
-        
-        vector<vector<int>>adj(n);
+        int n = isConnected.size();
 
-        for(int i=0;i<n;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(isConnected[i][j]==1 && i!=j)
-                { 
+        int m = isConnected[0].size();
 
+        vector<bool> vis(n, false);
+
+        unordered_map<int, vector<int>> adj;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (isConnected[i][j] == 1) {
                     adj[i].push_back(j);
                     adj[j].push_back(i);
-
                 }
             }
         }
 
+        int count = 0;
 
-        vector<int>vis(n,0);
+        for (int i = 0; i < vis.size(); i++) {
 
-        int ans=0;
-
-        for(int i=0;i<n;i++)
-        {
-           if(!vis[i])
-           {
-            
-            ans++;
-
-            dfs(i,vis,adj);
-
-           }
+            if (!vis[i]) {
+                dfs(adj, i, vis);
+                count++;
+            }
         }
 
-        return ans;
-
-
-        
-
-
-
-
+        return count;
     }
 };
