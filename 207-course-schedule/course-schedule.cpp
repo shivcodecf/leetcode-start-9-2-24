e4f5ac17eb8137af6eeb54f2efb16b67e2ef3f4e
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2026-05-18 16:51:32 +0530

    Time: 3 ms (75.77%) | Memory: 19.1 MB (90.90%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,65 +1,52 @@
 class Solution {
 public:
-    
-    bool topologicalSortCheck(unordered_map<int, vector<int>> &adj, int n, vector<int> &indegree) {
-        queue<int> que;
-        
-        int count = 0;
-        
-        for(int i = 0; i<n; i++) {
-            if(indegree[i] == 0) {
-                count++;
-                que.push(i);
-            }
-        }
-        
-        
-        while(!que.empty()) {
-            int u = que.front();
-            que.pop();
-            
-            for(int &v : adj[u]) {
-                
-                indegree[v]--;
-                
-                if(indegree[v] == 0) {
-                    count++;
-                    que.push(v);
-                }
-                
-            }
-            
-        }
-        
-        if(count == n) //I was able to visit all nodes (course)
-            return true; //i.e. I was able to finish all courses
-        
-        return false; //means there was a cycle and I couldn't complete all courses
-    }
-
-    
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        
-        unordered_map<int, vector<int>> adj;
-        
-        vector<int> indegree(numCourses, 0); //kahn's algo
-        
-        for(auto &vec : prerequisites) {
-            int a = vec[0];
-            int b = vec[1];
-            
-            //b ---> a
-            adj[b].push_back(a);
-            
-            //arrow ja raha hai 'a' me
-            indegree[a]++;
 
+        vector<vector<int>> adj(numCourses);
+        
+        vector<int> indegree(numCourses, 0);
+
+        // build graph
+        for(auto &p : prerequisites) {
+
+            int a = p[0];
+            int b = p[1];
+
+            // b -> a
+            adj[b].push_back(a);
+
+            indegree[a]++;
         }
-        
-        
-        
-        //if cycle is present, not possible
-        
-        return topologicalSortCheck(adj, numCourses, indegree);
+
+        queue<int> q;
+
+        // push indegree 0 nodes
+        for(int i = 0; i < numCourses; i++) {
+
+            if(indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        int count = 0;
+
+        while(!q.empty()) {
+
+            int node = q.front();
+            q.pop();
+
+            count++;
+
+            for(auto neigh : adj[node]) {
+
+                indegree[neigh]--;
+
+                if(indegree[neigh] == 0) {
+                    q.push(neigh);
+                }
+            }
+        }
+
+        return count == numCourses;
     }
 };
