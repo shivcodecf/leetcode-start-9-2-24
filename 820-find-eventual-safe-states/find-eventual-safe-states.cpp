507105b11c89154f5edbd347f33988c80bcd667e
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2026-05-18 18:27:56 +0530

    Time: 51 ms (40.59%) | Memory: 66.1 MB (46.67%) - LeetSync

--- 820-find-eventual-safe-states/find-eventual-safe-states.cpp
+++ 820-find-eventual-safe-states/find-eventual-safe-states.cpp
@@ -1,39 +1,53 @@
 class Solution {
 public:
-    bool isCycleDFS(vector<vector<int>>& adj, int u, vector<bool>& visited, vector<bool>& inRecursion) {
-        visited[u] = true;
-        inRecursion[u] = true;
-        
-        
-        for(int &v : adj[u]) {
-            //if not visited, then we check for cycle in DFS
-            if(visited[v] == false && isCycleDFS(adj, v, visited, inRecursion))
-                return true;
-            else if(inRecursion[v] == true)
-                return true;
-        }
-        
-        inRecursion[u] = false;
-        return false;
-        
-    }
-    
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int V = graph.size();
-        vector<bool> visited(V, false);
-        vector<bool> inRecursion(V, false);
-        
-        for(int i = 0; i<V; i++) {
-            if(!visited[i])
-                isCycleDFS(graph, i, visited, inRecursion);
+
+        int n = graph.size();
+
+        vector<vector<int>> revAdj(n);
+        vector<int> outdegree(n, 0);
+
+        // reverse graph
+        for(int i = 0; i < n; i++) {
+
+            outdegree[i] = graph[i].size();
+
+            for(auto neigh : graph[i]) {
+                revAdj[neigh].push_back(i);
+            }
         }
-        
-        vector<int> safeNodes;
-        for(int i = 0; i<V; i++) {
-            if(!inRecursion[i])
-                safeNodes.push_back(i);
+
+        queue<int> q;
+
+        // terminal nodes
+        for(int i = 0; i < n; i++) {
+
+            if(outdegree[i] == 0) {
+                q.push(i);
+            }
         }
-        
-        return safeNodes;
+
+        vector<int> ans;
+
+        while(!q.empty()) {
+
+            int node = q.front();
+            q.pop();
+
+            ans.push_back(node);
+
+            for(auto prev : revAdj[node]) {
+
+                outdegree[prev]--;
+
+                if(outdegree[prev] == 0) {
+                    q.push(prev);
+                }
+            }
+        }
+
+        sort(ans.begin(), ans.end());
+
+        return ans;
     }
 };
