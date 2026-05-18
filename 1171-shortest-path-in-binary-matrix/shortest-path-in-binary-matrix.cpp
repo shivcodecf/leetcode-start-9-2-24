cecf6e49fa55e04495595192cee9e5eec2671250
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2026-05-18 10:39:48 +0530

    Time: 20 ms (42.21%) | Memory: 26.4 MB (24.81%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -1,110 +1,87 @@
 class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-        
-      priority_queue<pair<int,pair<int,int>>,vector<pair<int,pair<int,int>>>,greater<>>pq;
 
-      int n = grid.size();
+        queue<pair<int, pair<int, int>>> q;
 
-      int m = grid[0].size();
+        int n = grid.size(), m = grid[0].size();
 
-      if(grid[0][0]== 1 || grid[n-1][m-1 ]== 1)
-      {
+        vector<vector<int>> vis(n, vector<int>(m, -1));
+
+        if(grid[0][0]==1)
+        {
+            return -1;
+        }
+
+        q.push({1, {0, 0}});
+
+        while (!q.empty()) {
+
+            int dis = q.front().first;
+            int x = q.front().second.first;
+            int y = q.front().second.second;
+            q.pop();
+
+            if (x == n - 1 && y == m - 1) {
+                return dis;
+            }
+
+            if (x + 1 < n && grid[x + 1][y] == 0 && vis[x + 1][y] == -1) {
+                vis[x + 1][y] = 1;
+                q.push({dis + 1, {x + 1, y}});
+            }
+
+            if (y + 1 < m && grid[x][y + 1] == 0 && vis[x][y + 1] == -1) {
+                vis[x][y + 1] = 1;
+                q.push({dis + 1, {x, y + 1}});
+            }
+
+            if (y - 1 >= 0 && grid[x][y - 1] == 0 && vis[x][y - 1] == -1) {
+                vis[x][y - 1] = 1;
+                q.push({dis + 1, {x, y - 1}});
+            }
+
+            if (x - 1 >= 0 && grid[x - 1][y] == 0 && vis[x - 1][y] == -1) {
+                vis[x - 1][y] = 1;
+                q.push({dis + 1, {x - 1, y}});
+            }
+
+            if (x - 1 >= 0 && y - 1 >= 0 && grid[x - 1][y - 1] == 0 &&
+                vis[x - 1][y - 1] == -1) {
+
+                vis[x - 1][y - 1] = 1;
+                q.push({dis + 1, {x - 1, y - 1}});
+            }
+
+            // if (x - 1 >= 0 && y - 1 >= 0 && grid[x - 1][y - 1] == 0 &&
+            //     vis[x - 1][y - 1] == 0) {
+            //     q.push({dis + 1, {x - 1, y - 1}});
+            // }
+
+            if (x + 1 < n && y + 1 < m && grid[x + 1][y + 1] == 0 &&
+                vis[x + 1][y + 1] == -1) {
+
+                vis[x + 1][y + 1] = 1;
+                q.push({dis + 1, {x + 1, y + 1}});
+            }
+
+            if (x - 1 >= 0 && y + 1 < m && grid[x - 1][y + 1] == 0 &&
+                vis[x - 1][y + 1] == -1) {
+
+                vis[x - 1][y + 1] = 1;
+                q.push({dis + 1, {x - 1, y + 1}});
+            }
+
+            if (x + 1 < n && y - 1 >= 0 && grid[x + 1][y - 1] == 0 &&
+                vis[x + 1][y - 1] == -1) {
+
+                vis[x + 1][y - 1] = 1;
+
+                q.push({dis + 1, {x + 1, y - 1}});
+
+            }
+        }
+
         return -1;
-      }
-
-      vector<vector<int>>dis(n+1,vector<int>(m+1,1e9));
-
-      dis[0][0] = 0;
-
-      pq.push({0,{0,0}});
-
-      while(!pq.empty())
-      { 
-
-        auto it = pq.top();
-
-        pq.pop();
-
-        int path = it.first;
-
-        int row = it.second.first;
-
-        int col = it.second.second;
-
-        if(row == n-1 && col == m-1)
-        {
-            return path+1;
-        }
-
-        int newR = 0, newC = 0;
-
-        if(row-1 >=0 && col-1>=0 && row-1<n && col-1<m && path+1<dis[row-1][col-1] && grid[row-1][col-1]==0)
-
-        {
-            dis[row-1][col-1] = path+1;
-            pq.push({path+1,{row-1,col-1}});
-        }
-
-         if(row-1 >=0 && col+1>=0 && row-1<n && col+1<m && path+1<dis[row-1][col+1] && grid[row-1][col+1]==0)
-
-        {
-            dis[row-1][col+1] = path+1;
-            pq.push({path+1,{row-1,col+1}});
-        }
-
-         if(row+1 >=0 && col+1>=0 && row+1<n && col+1<m && path+1<dis[row+1][col+1] && grid[row+1][col+1]==0)
-
-        {
-            dis[row+1][col+1] = path+1;
-            pq.push({path+1,{row+1,col+1}});
-        }
-
-          if(row+1 >=0 && col-1>=0 && row+1<n && col-1<m && path+1<dis[row+1][col-1] && grid[row+1][col-1]==0)
-
-        {
-            dis[row+1][col-1] = path+1;
-            pq.push({path+1,{row+1,col-1}});
-        }
-
-           if(row >=0 && col+1>=0 && row<n && col+1<m && path+1<dis[row][col+1] && grid[row][col+1]==0 )
-
-        {
-            dis[row][col+1] = path+1;
-            pq.push({path+1,{row,col+1}});
-        }
-
-           if(row +1 >=0 && col>=0 && row+1<n && col<m && path+1<dis[row+1][col] && grid[row+1][col]==0)
-
-        {
-            dis[row+1][col] = path+1;
-            pq.push({path+1,{row+1,col}});
-        }
-
-         if(row >=0 && col-1>=0 && row<n && col-1<m && path+1<dis[row][col-1] && grid[row][col-1]==0)
-
-        {
-            dis[row][col-1] = path+1;
-            pq.push({path+1,{row,col-1}});
-        }
-
-          if(row -1 >=0 && col>=0 && row-1<n && col<m && path+1<dis[row-1][col] && grid[row-1][col]==0)
-
-        {
-            dis[row-1][col] = path+1;
-            pq.push({path+1,{row-1,col}});
-        }
-
-      }
-
-      return -1;
-
-
-
-
-
-
-
-
     }
 };
