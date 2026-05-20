5ae6535d10cd48e3f93a2d774c1f5d1be0a44fd5
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2026-05-20 11:14:34 +0530

    Time: 3 ms (89.41%) | Memory: 23.9 MB (32.35%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -2,51 +2,40 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
 
-        vector<int> newIntervals;
-
         int n = intervals.size();
 
-        sort(intervals.begin(), intervals.end());
+        sort(intervals.begin(),intervals.end());
+
+        vector<vector<int>> ans;
+
+        vector<int> newIntervals;
 
         newIntervals.push_back(intervals[0][0]);
 
         newIntervals.push_back(intervals[0][1]);
 
-        vector<vector<int>> ans;
+        // new = [2,8] , intervals =  [10,15]  
 
-        for (int i = 0; i < n; i++) {
-            cout << intervals[i][0] << " " << intervals[i][1] << endl;
-        }
+        // intervals = [10,15] , newIntervals = [20,30]
 
-        int i = 1;
 
-        while (i < n) {
+        for (int i = 1; i < n; i++) {
 
-            if (intervals[i][0] > newIntervals[1]) {
+            if (newIntervals[1] < intervals[i][0]) {
                 ans.push_back(newIntervals);
                 newIntervals[0] = intervals[i][0];
                 newIntervals[1] = intervals[i][1];
-            }
-
-            if (intervals[i][1] < newIntervals[0]) {
-
+            } else if (newIntervals[0] > intervals[i][1]) {
                 ans.push_back(intervals[i]);
-               
-
             } else {
                 newIntervals[0] = min(newIntervals[0], intervals[i][0]);
                 newIntervals[1] = max(newIntervals[1], intervals[i][1]);
             }
-
-            i++;
         }
 
         ans.push_back(newIntervals);
 
-        // while (i < n) {
-        //     ans.push_back(intervals[i]);
-        //     i++;
-        // }
+
 
         return ans;
     }
