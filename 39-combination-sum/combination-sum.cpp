3bb54da36a6c847251340bde0ae20b93d2d4168f
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2026-05-21 15:08:28 +0530

    Time: 2 ms (56.64%) | Memory: 14.3 MB (35.93%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,58 +1,40 @@
 class Solution {
-    vector<vector<int>> ans;
 
-    void solve(vector<int>& candidates, int target, vector<int>& temp, int sum,
-               int ind) {
+    vector<vector<int>>ans;
 
-        int n = candidates.size();
-
-        if (sum > target) {
-            return;
-        }
-
-        if (ind >= n) {
-            if (sum == target) {
-                ans.push_back(temp);
-            }
-            return;
-        }
+     void solve(vector<int>& candidates, int n, vector<int>& v, int target, int sum,
+          int ind) {
 
         if (sum == target) {
-            ans.push_back(temp);
+            ans.push_back(v);
             return;
         }
 
-        solve(candidates, target, temp, sum, ind + 1);
-
-        temp.push_back(candidates[ind]);
-
-        solve(candidates, target, temp, sum + candidates[ind], ind);
-
-        temp.pop_back();
-
-        // sum += candidates[ind];
-
-        // Skip all duplicates of the current value
-        while (ind + 1 < candidates.size() &&
-               candidates[ind] == candidates[ind + 1]) {
-            ind++;
+        if (ind >= n || sum > target) {
+            return;
         }
 
-        
+        v.push_back(candidates[ind]);
 
-        // sum -= candidates[ind];
+        solve(candidates, n, v, target, sum+candidates[ind], ind);
 
-        // solve(candidates, target, temp, sum + candidates[ind], ind + 1);
+        v.pop_back();
+
+        solve(candidates, n, v, target, sum, ind + 1);
+
+
     }
 
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
 
-        vector<int> temp;
+        vector<int> v;
+
+        int n = candidates.size();
 
         sort(candidates.begin(), candidates.end());
 
-        solve(candidates, target, temp, 0, 0);
+        solve(candidates, n, v, target, 0, 0);
 
         return ans;
     }
