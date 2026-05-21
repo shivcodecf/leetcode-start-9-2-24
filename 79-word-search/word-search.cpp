3663ef5c4338c714bce29718d1723c0e286aa8aa
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2026-05-21 16:38:07 +0530

    Time: 163 ms (95.29%) | Memory: 10.6 MB (65.38%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,48 +1,61 @@
 class Solution {
 
-    bool solve(vector<vector<char>>& board, string& word, int ind, int i, int j,
-               vector<vector<char>>& vis) {
+    bool solve(vector<vector<char>>& board, string& word, int n, int m, int i,
+               int j, vector<vector<char>>& vis, int ind) {
 
-        if (ind >= word.size()) {
+        int l = word.size();
+
+        if (ind >= l) {
             return true;
         }
 
-        
+        if (j + 1 < m && word[ind] == board[i][j + 1] &&
+            vis[i][j + 1] == '.') {
 
-        int n = board.size(), m = board[0].size();
+            vis[i][j + 1] = board[i][j + 1];
 
-        if (i + 1 < n && board[i + 1][j] == word[ind] && vis[i + 1][j] == '#') {
-            vis[i + 1][j] = word[ind];
-            if(solve(board, word, ind + 1, i + 1, j, vis)){
+            if (solve(board, word, n, m, i, j + 1, vis, ind + 1)) {
                 return true;
             }
-            vis[i + 1][j] = '#';
+
+            vis[i][j + 1] = '.';
+
         }
 
-        if (j + 1 < m && board[i][j + 1] == word[ind] && vis[i][j + 1] == '#') {
-            vis[i][j + 1] = word[ind];
-            if(solve(board, word, ind + 1, i, j + 1, vis)){
+        if (i - 1 >= 0 && word[ind] == board[i - 1][j] &&
+            vis[i - 1][j] == '.') {
+
+            vis[i - 1][j] = board[i - 1][j];
+
+            if (solve(board, word, n, m, i - 1, j, vis, ind + 1)) {
                 return true;
             }
-            vis[i][j + 1] = '#';
+
+            vis[i - 1][j] = '.';
         }
 
-        if (i - 1 >= 0 && board[i - 1][j] == word[ind] &&
-            vis[i - 1][j] == '#') {
-            vis[i - 1][j] = word[ind];
-            if(solve(board, word, ind + 1, i - 1, j, vis)){
+        if (i + 1 < n && word[ind] == board[i + 1][j] &&
+            vis[i + 1][j] == '.') {
+
+            vis[i + 1][j] = board[i + 1][j];
+
+            if (solve(board, word, n, m, i + 1, j, vis, ind + 1)) {
                 return true;
             }
-             vis[i-1][j] = '#';
+
+            vis[i + 1][j] = '.';
         }
 
-        if (j - 1 >= 0 && board[i][j - 1] == word[ind] &&
-            vis[i][j - 1] == '#') {
-            vis[i][j - 1] = word[ind];
-            if(solve(board, word, ind + 1, i, j - 1, vis)){
+        if (j - 1 >= 0 && word[ind] == board[i][j - 1] &&
+            vis[i][j - 1] == '.') {
+
+            vis[i][j - 1] = board[i][j - 1];
+
+            if (solve(board, word, n, m, i, j - 1, vis, ind + 1)) {
                 return true;
             }
-            vis[i][j - 1] = '#';
+
+            vis[i][j - 1] = '.';
         }
 
         return false;
@@ -53,16 +66,19 @@ public:
 
         int n = board.size(), m = board[0].size();
 
-        vector<vector<char>> vis(n, vector<char>(m, '#'));
+        vector<vector<char>> vis(n, vector<char>(m, '.'));
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (board[i][j] == word[0]) {
-                    vis[i][j] = word[0];
-                    if (solve(board, word, 1, i, j, vis)) {
+                if (word[0] == board[i][j]) {
+
+                    vis[i][j] = board[i][j];
+
+                    if (solve(board, word, n, m, i, j, vis, 1)) {
                         return true;
                     }
-                    vis[i][j] = '#';
+
+                    vis[i][j] = '.';
                 }
             }
         }
