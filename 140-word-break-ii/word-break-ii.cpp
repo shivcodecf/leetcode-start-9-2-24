541b48964535e67908916a7dfa67da8b33dbe8f0
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2026-05-26 13:36:22 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (62.06%) - LeetSync

--- 140-word-break-ii/word-break-ii.cpp
+++ 140-word-break-ii/word-break-ii.cpp
@@ -1,25 +1,59 @@
 class Solution {
-public: 
+
     vector<string> ans;
-    void rec(string &s, set<string> &dict, int ind, string ds){
-        if(ind>=s.size()){
-            ds.pop_back();
-            ans.push_back(ds);
+    map<string, int> mp;
+
+    void solve(string s, vector<string>& wordDict, int ind, vector<string>& v) {
+
+       if(ind >= s.size())
+        {
+            string sentence = "";
+
+            for(int i = 0; i < v.size(); i++)
+            {
+                sentence += v[i];
+
+                if(i != v.size() - 1)
+                {
+                    sentence += " ";
+                }
+            }
+
+            ans.push_back(sentence);
+
             return;
         }
-        string word="";
-        for(int i=ind;i<s.size();i++){
-            word+= s[i];
-            if(dict.find(word)!=dict.end()){
-                rec(s,dict,i+1,ds+word+" ");
-               
+
+
+        string s1 = "";
+
+        for (int i = ind; i < s.size(); i++) {
+            s1 += s[i];
+
+            if (mp.find(s1) != mp.end()) {
+                v.push_back(s1);
+                solve(s, wordDict, i + 1, v);
+                v.pop_back();
             }
         }
+
+
+
     }
-    vector<string> wordBreak(string &s, vector<string>& wordDict) {
-        set<string> dict(wordDict.begin(),wordDict.end());
-        rec(s,dict,0,"");
+
+public:
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+
+        for (int i = 0; i < wordDict.size(); i++) {
+            mp[wordDict[i]]++;
+        }
+
+        vector<string> v;
+
+        solve(s, wordDict, 0, v);
+
         return ans;
+
 
     }
 };
