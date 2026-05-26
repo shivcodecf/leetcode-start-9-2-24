dd17f0534cedc512666474909f829b3f5996a277
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2026-05-26 15:46:38 +0530

    Time: 628 ms (5.05%) | Memory: 296.5 MB (18.08%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,26 +1,28 @@
 class Solution {
-
-    int solve(vector<int>& nums, int ind, int prev, vector<vector<int>>& dp) {
+    int solve(vector<int>& nums,int ind,int prev,vector<vector<int>>& dp)
+    {   
         int n = nums.size();
 
-        if (ind >= n) {
+        if(ind>=n)
+        {
             return 0;
         }
 
-        if (dp[ind][prev+1] != -1) {
+        int take = INT_MIN , notake = INT_MIN;
+
+        if(dp[ind][prev+1]!=-1)
+        {
             return dp[ind][prev+1];
         }
 
-        int take = 0;
-
-        int skip = solve(nums, ind + 1, prev, dp);
-
-        if (prev == -1 || nums[ind] > nums[prev]) {
-            take = 1 + solve(nums, ind + 1, ind, dp);
+        if(prev == -1 || nums[ind]>nums[prev])
+        {
+           take = 1+solve(nums,ind+1,ind,dp);
         }
 
-        return dp[ind][prev+1] = max(skip, take);
+        notake = solve(nums,ind+1,prev,dp);
 
+        return dp[ind][prev+1] =  max(take,notake);
     }
 
 public:
@@ -28,8 +30,9 @@ public:
 
         int n = nums.size();
 
-        vector<vector<int>> dp(n , vector<int>(n + 1, -1));
+        vector<vector<int>>dp(n,vector<int>(n+1,-1));
 
-        return solve(nums, 0, -1, dp);
+        return solve(nums,0,-1,dp);
+        
     }
 };
