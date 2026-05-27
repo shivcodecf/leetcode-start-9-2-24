02ff1a8be90eb8b42564490db23b5abc6d6b00a7
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2026-05-27 17:36:33 +0530

    Time: 89 ms (43.56%) | Memory: 182.1 MB (39.23%) - LeetSync

--- 146-lru-cache/lru-cache.cpp
+++ 146-lru-cache/lru-cache.cpp
@@ -1,59 +1,65 @@
 class LRUCache {
 public:
+
+   list<int>dll;
+
+   unordered_map<int,pair<list<int>::iterator,int>>mp;
+    
     int capacity;
 
-    list<int> dll;
-
-    map<int, pair<list<int>::iterator, int>> mp;
-
-    // map<int,list<pair<int>::iterator,int>>mp;
-
-    void recentlyMark(int key) {
-
-        dll.erase(mp[key].first);
-
-        dll.push_front(key);
-
-        mp[key].first = dll.begin();
+    void recentlyUsed(int key)
+    {
+      dll.erase(mp[key].first);
+      dll.push_front(key);
+      mp[key].first = dll.begin(); 
     }
 
-    LRUCache(int capacity) { this->capacity = capacity; }
 
+    LRUCache(int capacity) {
+        this->capacity = capacity;
+    }
+    
     int get(int key) {
-        if (mp.find(key) != mp.end()) {
-            recentlyMark(key);
-           return  mp[key].second;
-        }
-        return -1;
+
+     if(mp.find(key)!=mp.end())
+     { 
+         recentlyUsed(key);
+         
+        return mp[key].second;
+
+       
+     }
+
+     return -1;
+    
     }
-
+    
     void put(int key, int value) {
-        if (mp.find(key) != mp.end()) {
 
+        if(mp.find(key)!=mp.end())
+        {
             mp[key].second = value;
-
-            recentlyMark(key);
+            recentlyUsed(key);
         }
 
         else {
 
-            dll.push_front(key);
+           dll.push_front(key);
 
-            mp[key] = {dll.begin(), value};
+           mp[key] = {dll.begin(),value};  
 
-            capacity--;
-        }
+           capacity--;
 
-        if (capacity < 0) {
-
-            int key1 = dll.back();
+           if(capacity<0)
+           { 
 
             mp.erase(dll.back());
-
             dll.pop_back();
+            
+           }
 
-            capacity++;
         }
+        
     }
 };
 
