a55c4b629b2bcdc30f1952278cb7dc016bf498e9
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2026-06-02 21:09:09 +0530

    Time: 272 ms (22.19%) | Memory: 137.8 MB (11.62%) - LeetSync

--- 3742-maximum-number-of-items-from-sale-i/maximum-number-of-items-from-sale-i.cpp
+++ 3742-maximum-number-of-items-from-sale-i/maximum-number-of-items-from-sale-i.cpp
@@ -1,34 +1,63 @@
 class Solution {
-public:
-    int dp[1000][1501];
-    vector<int> free;
-    int mini, n;
-    
-    int solve(int i, int tot, vector<vector<int>>& items) {
-        if(i>=items.size()) return tot/mini;
-        if(dp[i][tot]!=-1) return dp[i][tot];
+    int ans = 0;
 
-        int take=0;
-        if(tot>=items[i][1]) {
-            take=free[i]+solve(i+1, tot-items[i][1], items);
+    vector<int> free;
+
+    int mini = INT_MAX;
+
+    int solve(vector<vector<int>>& items, int budget, int ind,  vector<vector<int>>& dp)
+
+    {
+        int n = items.size();
+
+        if (ind >= n) {
+            return budget / mini;
         }
 
-        int skip=solve(i+1, tot, items);
-        return dp[i][tot]=max(take, skip);
+        int take = INT_MIN, notake = INT_MIN;
+
+        if(dp[budget][ind]!=-1)
+        {
+            return dp[budget][ind];
+        }
+
+        if (budget >= items[ind][1]) {
+
+            take = free[ind] + solve(items, budget - items[ind][1], ind + 1,dp);
+        }
+
+        notake = solve(items, budget, ind + 1,dp);
+
+        // max(take, notake);
+
+        return dp[budget][ind] =  max(take, notake);
     }
-    
+
+public:
     int maximumSaleItems(vector<vector<int>>& items, int budget) {
-        mini=INT_MAX, n=items.size();
+
+        // sort(items.begin(), items.end());
+
+        int n = items.size();
+
+        vector<vector<int>>dp(budget+1,vector<int>(n,-1));
+
+
+
         free.resize(n, 0);
 
-        for(int i=0; i<n; i++) {
-            mini=min(mini, items[i][1]);
-            for(int j=0; j<n; j++) {
-                if(items[j][0]%items[i][0] == 0) free[i]++;
+        for (int i = 0; i < n; i++) {
+
+            mini = min(mini, items[i][1]);
+
+            for (int j = 0; j < n; j++) {
+
+                if (items[j][0] % items[i][0] == 0) {
+                    free[i]++;
+                }
             }
         }
 
-        memset(dp, -1, sizeof(dp));
-        return solve(0, budget, items);
+        return solve(items, budget, 0,dp);
     }
 };
