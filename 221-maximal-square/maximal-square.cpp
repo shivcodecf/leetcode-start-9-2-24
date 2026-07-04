a252e60ac95561a31bc0d6e88f2e97c30f80ef71
Shivam yadav <113226699+shivcodecf@users.noreply.github.com>
2026-07-04 10:54:58 +0530

    Time: 32 ms (12.66%) | Memory: 31.1 MB (17.50%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -1,26 +1,41 @@
 class Solution {
 public:
-    int maximalSquare(vector<vector<char>>& matrix) {
-    int n = matrix.size();
-    if (n == 0) return 0;
-    int m = matrix[0].size();
-    vector<vector<int>> dp(n, vector<int>(m, 0));
-    int ans = 0;
+    int m, n;
+    vector<vector<int>> dp;
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
-            if (matrix[i][j] == '1') {
-                if (i == 0 || j == 0) {
-                    dp[i][j] = matrix[i][j]-'0'; // If it's the first row or first column, the max square is 1
-                } else {
-                    dp[i][j] = min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]}) + 1;
-                }
-                ans = max(ans, dp[i][j]);
-            }
-        }
+    int solve(int i, int j, vector<vector<char>>& matrix) {
+
+        if (i >= m || j >= n)
+            return 0;
+
+        if (dp[i][j] != -1)
+            return dp[i][j];
+
+        if (matrix[i][j] == '0')
+            return dp[i][j] = 0;
+
+        int down = solve(i + 1, j, matrix);
+        int right = solve(i, j + 1, matrix);
+        int diag = solve(i + 1, j + 1, matrix);
+
+        return dp[i][j] = 1 + min({down, right, diag});
     }
 
-    return ans * ans; // Returning the area of the largest square
-}
+    int maximalSquare(vector<vector<char>>& matrix) {
 
+        m = matrix.size();
+        n = matrix[0].size();
+
+        dp.assign(m, vector<int>(n, -1));
+
+        int ans = 0;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                ans = max(ans, solve(i, j, matrix));
+            }
+        }
+
+        return ans * ans;
+    }
 };
